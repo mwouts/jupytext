95e11ebe240b5ae882399c96c2d0775084a47262
Marc Wouts <marc.wouts@gmail.com>
2018-06-16 01:13:21 +0200

    Don't add blank spaces between markdown and code cells. This way the Rmd two way conversion is preserved.

--- nbrmd/nbrmd.py
+++ nbrmd/nbrmd.py
@@ -46,12 +46,6 @@ class RmdReader(NotebookReader):
     def reads(self, s, **kwargs):
         return self.to_notebook(s, **kwargs)
 
-    def markdown_cell(self, lines):
-        if len(lines) >= 1:
-            if lines[-1] == u'':
-                lines = lines[:-1]
-        return new_markdown_cell(source=u'\n'.join(lines))
-
     def to_notebook(self, s, **kwargs):
         lines = s.splitlines()
 
@@ -78,7 +72,7 @@ class RmdReader(NotebookReader):
             if state is State.MARKDOWN:
                 if _start_code_re.match(line):
                     if len(cell_lines):
-                        cells.append(self.markdown_cell(cell_lines))
+                        cells.append(new_markdown_cell(source=u'\n'.join(cell_lines)))
 
                     chunk_options = _start_code_re.findall(line)[0]
                     cell_lines = []
@@ -93,8 +87,7 @@ class RmdReader(NotebookReader):
                     state = State.MARKDOWN
                     continue
 
-            if state is not State.MARKDOWN or line != u'' or len(cell_lines):
-                cell_lines.append(line)
+            cell_lines.append(line)
 
         # Append last cell if not empty
         if state is State.MARKDOWN:
@@ -115,18 +108,18 @@ class RmdWriter(NotebookWriter):
         lines = []
         for cell in nb.cells:
             if cell.cell_type == u'code':
-                input = cell.get(u'source')
                 chunk_options = cell.get(u'metadata', {}).get(u'chunk_options', 'python')
-                if input is not None:
-                    lines.append(u'```{' + chunk_options + '}')
-                    lines.extend(input.splitlines())
-                    lines.append(u'```')
-                    lines.append(u'')
-            elif cell.cell_type == u'markdown' or cell.cell_type == u'raw':
+                lines.append(u'```{' + chunk_options + '}')
                 input = cell.get(u'source')
                 if input is not None:
                     lines.extend(input.splitlines())
-                    lines.append(u'')
+                lines.append(u'```')
+            elif cell.cell_type == u'markdown' or cell.cell_type == u'raw':
+                lines.append(cell.get(u'source', ''))
+
+        # End file with a line return
+        lines.append('')
+
         return u'\n'.join(lines)
 
 
