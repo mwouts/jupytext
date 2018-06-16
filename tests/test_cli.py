cd9178d81f9a2d76fc1f8b3a97a8619181f543fd
Marc Wouts <marc.wouts@gmail.com>
2018-06-17 01:44:00 +0200

    fix tests on python 2.7 / 3.4

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -8,7 +8,7 @@ from tests.utils import list_all_notebooks, filter_output_and_compare_notebooks
 
 @pytest.mark.parametrize('nb_file', list_all_notebooks('.ipynb') + list_all_notebooks('.Rmd'))
 def test_convert_single_file(nb_file, tmpdir):
-    nb_org = os.path.join(tmpdir, os.path.basename(nb_file))
+    nb_org = tmpdir.join(os.path.basename(nb_file))
     base, ext = os.path.splitext(nb_org)
     nb_other = base + '.ipynb' if ext == '.Rmd' else base + '.Rmd'
 
@@ -27,7 +27,7 @@ def test_convert_multiple_file(nb_files, tmpdir):
     nb_others = []
 
     for nb_file in nb_files:
-        nb_org = os.path.join(tmpdir, os.path.basename(nb_file))
+        nb_org = tmpdir.join(os.path.basename(nb_file))
         base, ext = os.path.splitext(nb_org)
         nb_other = base + '.ipynb' if ext == '.Rmd' else base + '.Rmd'
         copyfile(nb_file, nb_org)
--- tests/test_jupyter_hook.py
+++ tests/test_jupyter_hook.py
@@ -1,5 +1,4 @@
 import pytest
-import os
 import nbrmd
 from tests.utils import list_all_notebooks, filter_output_and_compare_notebooks
 
@@ -7,8 +6,8 @@ from tests.utils import list_all_notebooks, filter_output_and_compare_notebooks
 @pytest.mark.parametrize('nb_file', list_all_notebooks('.ipynb'))
 def test_pre_save_hook(nb_file, tmpdir):
     nb = nbrmd.readf(nb_file)
-    tmp_ipynb = os.path.join(tmpdir, 'notebook.ipynb')
-    tmp_rmd = os.path.join(tmpdir, 'notebook.Rmd')
+    tmp_ipynb = tmpdir.join('notebook.ipynb')
+    tmp_rmd = tmpdir.join('notebook.Rmd')
 
     nbrmd.jupyter_hook.pre_save_hook(model=dict(type='notebook', content=nb), path=tmp_ipynb)
 
