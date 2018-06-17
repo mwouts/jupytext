18d51c44d056682d6b613e1fa6cd9d556f1015bc
Marc Wouts <marc.wouts@gmail.com>
2018-06-17 02:38:58 +0200

    str on tmpdir

--- tests/test_nbrmd_errors.py
+++ tests/test_nbrmd_errors.py
@@ -22,11 +22,11 @@ def test_read_wrong_ext(tmpdir, nb_file='notebook.ext'):
     nb_file = tmpdir.join('nb_file')
     nb_file.write('{}')
     with pytest.raises(TypeError):
-        nbrmd.readf(nb_file)
+        nbrmd.readf(str(nb_file))
 
 
 def test_write_wrong_ext(tmpdir, nb_file='notebook.ext'):
-    nb_file = tmpdir.join('nb_file')
+    nb_file = str(tmpdir.join('nb_file'))
     with pytest.raises(TypeError):
         nbrmd.writef(dict(), nb_file)
 
