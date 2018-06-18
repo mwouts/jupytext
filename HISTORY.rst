a3ee8fc24b3ce4d91ba38eae6fc23de052245fa8
Marc Wouts <marc.wouts@gmail.com>
2018-06-19 00:38:10 +0200

    Version bump

---
+++ HISTORY.rst
@@ -0,0 +1,20 @@
+.. :changelog:
+
+Release History
+---------------
+
+dev
++++
+
+0.1.1 (2018-06-19)
++++++++++++++++++++
+
+**Improvements**
+- ``nbrmd`` prints the result of conversion to stdout, unless flag ``-i`` is provided
+- Notebooks with R code chunks are supported
+
+0.1 (2018-06-18)
++++++++++++++++++++
+
+- Initial version with the ``nbrmd`` converter and Jupyter ``pre_save_hook``
+
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from nbrmd.nbrmd import readme
 
 setup(
     name='nbrmd',
-    version='0.1',
+    version='0.1.1',
     author='Marc Wouts',
     author_email='marc.wouts@gmail.com',
     description='Jupyter from/to R markdown notebooks',
@@ -19,5 +19,10 @@ setup(
                  'Framework :: Jupyter',
                  'Intended Audience :: Science/Research',
                  'Programming Language :: Python',
-                 'Topic :: Text Processing :: Markup')
+                 'Topic :: Text Processing :: Markup',
+                 'Programming Language :: Python',
+                 'Programming Language :: Python :: 2.7',
+                 'Programming Language :: Python :: 3',
+                 'Programming Language :: Python :: 3.4',
+                 'Programming Language :: Python :: 3.6')
 )
