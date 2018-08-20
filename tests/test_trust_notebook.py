2f6972777ba2b584286c07e70a0d61bd0bb2a74a
Marc Wouts <marc.wouts@gmail.com>
2018-08-21 00:08:05 +0200

    Save notebook local name

--- tests/test_trust_notebook.py
+++ tests/test_trust_notebook.py
@@ -28,7 +28,8 @@ def test_ipynb_notebooks_can_be_trusted(nb_file, tmpdir):
     cm = RmdFileContentsManager()
     root, file = os.path.split(nb_file)
     tmp_ipynb = str(tmpdir.join(file))
-    tmp_py = tmp_ipynb.replace('.ipynb', '.py')
+    py_file = file.replace('.ipynb', '.py')
+    tmp_py = str(tmpdir.join(py_file))
     shutil.copy(nb_file, tmp_ipynb)
 
     cm.default_nbrmd_formats = 'ipynb,py'
@@ -37,7 +38,7 @@ def test_ipynb_notebooks_can_be_trusted(nb_file, tmpdir):
 
     # Sign notebook explicitely (save it, and reload without
     # validating to remove 'trusted' metadata in cells)
-    cm.save(nb, tmp_py)
+    cm.save(nb, py_file)
     nb = cm._read_notebook(tmp_py)
     cm.notary.sign(nb)
 
@@ -45,7 +46,7 @@ def test_ipynb_notebooks_can_be_trusted(nb_file, tmpdir):
     assert cm.notary.check_cells(nb['content'])
 
     # Remove py file, content should be the same
-    os.remove(os.path.join(root, tmp_py))
+    os.remove(tmp_py)
     nb2 = cm.get(file)
     assert cm.notary.check_cells(nb2['content'])
 
