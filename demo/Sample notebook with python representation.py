749b7cd5aa62c5a9e4bb70f125fb4976455d5c9c
Marc Wouts <marc.wouts@gmail.com>
2018-08-21 02:28:52 +0200

    Jupyter magics now escaped

--- demo/Sample notebook with python representation.py
+++ demo/Sample notebook with python representation.py
@@ -33,7 +33,7 @@
 # 4. Now, modify the python file, save, and refresh the Jupyter notebook. Observe how inputs were updated, outputs preserved when possible, and kernel was left unchanged.
 # 5. Browse the github repo using Jupyter in binder, and open arbitrary python files as notebooks. Run some of them when applicable (test `filled_step.py` for instance).
 
-%matplotlib inline
+# %matplotlib inline
 
 
 import matplotlib.pyplot as plt
