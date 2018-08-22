9cc3c32ee6103e1239fdc8937b4b7d042947d0d8
Marc Wouts <marc.wouts@gmail.com>
2018-08-22 22:31:25 +0200

    Improved coverage and string parser

--- tests/test_stringparser.py
+++ tests/test_stringparser.py
@@ -20,3 +20,24 @@ and it ends here'''
         sp.read_line(line)
 
     assert quoted == [1, 2, 3, 4, 5, 6]
+
+
+def test_single_chars(text="""'This is a single line comment'''
+'and another one'
+# and comments
+"and line breaks"
+
+
+"and it ends here'''"
+
+
+1 + 1
+"""):
+    quoted = []
+    sp = StringParser('python')
+    for i, line in enumerate(text.splitlines()):
+        if sp.is_quoted():
+            quoted.append(i)
+        sp.read_line(line)
+
+    assert quoted == []
