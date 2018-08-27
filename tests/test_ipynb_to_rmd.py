30031567f12ec64f6ce4d1217cced3e63234f8b1
Marc Wouts <marc.wouts@gmail.com>
2018-08-27 23:38:49 +0200

    Removed duplicate test

--- tests/test_ipynb_to_rmd.py
+++ tests/test_ipynb_to_rmd.py
@@ -24,21 +24,3 @@ def test_identity_source_write_read(nb_file):
     nb2 = nbrmd.reads(rmd)
 
     compare(remove_outputs(nb1), remove_outputs(nb2))
-
-
-@pytest.mark.parametrize('nb_file', list_all_notebooks('.ipynb'))
-def test_identity_source_write_read_py(nb_file):
-    """
-    Test that writing the notebook with rmd, and read again,
-    is the same as removing outputs
-    :param file:
-    :return:
-    """
-
-    with open(nb_file) as fp:
-        nb1 = nbformat.read(fp, as_version=4)
-
-    md = nbrmd.writes(nb1, ext='.py')
-    nb2 = nbrmd.reads(md, ext='.py')
-
-    compare(remove_outputs_and_header(nb1), remove_outputs_and_header(nb2))
