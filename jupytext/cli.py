8597007c66cef2aa8cf433f8b588df8a2bf87b27
Marc Wouts <marc.wouts@gmail.com>
2018-09-05 23:44:22 +0200

    --to and --output options in jupytext command #46

--- jupytext/cli.py
+++ jupytext/cli.py
@@ -10,51 +10,45 @@ from .compare import test_round_trip_conversion
 from .file_format_version import check_file_version
 
 
-def convert_notebook_files(nb_files, nb_dest,
+def convert_notebook_files(nb_files, ext, output=None,
                            test_round_trip=False, preserve_outputs=True):
     """
     Export R markdown notebooks, python or R scripts, or Jupyter notebooks,
     to the opposite format
     :param nb_files: one or more notebooks files
-    :param nb_dest: destination file, extension ('.py') or format ('py')
+    :param ext: extension of destination
+    :param output: None, destination file, or '-' for stdout
     :param test_round_trip: should round trip conversion be tested?
     :param preserve_outputs: preserve the current outputs of .ipynb file
     when possible
     :return:
     """
 
-    if len(nb_files) > 1 and nb_dest not in NOTEBOOK_EXTENSIONS:
+    if len(nb_files) > 1 and output:
         raise ValueError(
-            "Converting multiple files requires "
-            "that destination be one of '{}'".format(
-                "', '".join(NOTEBOOK_EXTENSIONS)))
+            "Output argument can only be used with a single notebook")
 
     for nb_file in nb_files:
-        file, current_ext = os.path.splitext(nb_file)
+        dest, current_ext = os.path.splitext(nb_file)
         if current_ext not in NOTEBOOK_EXTENSIONS:
             raise TypeError('File {} is not a notebook'.format(nb_file))
 
         notebook = readf(nb_file)
-        dest, dest_ext = os.path.splitext(nb_dest)
-        if not dest_ext:
-            dest = file
-            if nb_dest in NOTEBOOK_EXTENSIONS:
-                dest_ext = nb_dest
-            else:
-                dest_ext = '.' + nb_dest
-
-        if dest_ext not in NOTEBOOK_EXTENSIONS:
-            raise TypeError('Destination extension {} is not a notebook'
-                            .format(dest_ext))
-
         if test_round_trip:
-            test_round_trip_conversion(notebook, dest_ext, preserve_outputs)
+            test_round_trip_conversion(notebook, ext, preserve_outputs)
+            continue
 
-        if '.' in nb_dest:
-            save_notebook_as(notebook, nb_file, dest + dest_ext,
-                             preserve_outputs)
-        elif not test_round_trip:
-            print(writes(notebook, ext=dest_ext))
+        if output == '-':
+            print(writes(notebook, ext=ext))
+            continue
+
+        if output:
+            dest, dest_ext = os.path.splitext(output)
+            if dest_ext != ext:
+                raise TypeError('Destination extension {} is not consistent'
+                                'with format {} '.format(dest_ext, ext))
+
+        save_notebook_as(notebook, nb_file, dest + ext, preserve_outputs)
 
 
 def save_notebook_as(notebook, nb_file, nb_dest, combine):
@@ -72,28 +66,67 @@ def cli_jupytext(args=None):
     """Command line parser for jupytext"""
     parser = argparse.ArgumentParser(
         description='Jupyter notebooks as markdown documents, '
-                    'Python or R scripts')
+                    'Julia, Python or R scripts')
+
+    parser.add_argument(
+        '--to',
+        choices=['notebook', 'python', 'julia', 'r', 'markdown',
+                 'rmarkdown'] + [ext.replace('.', '') for ext in
+                                 NOTEBOOK_EXTENSIONS],
+        help="Destination format")
     parser.add_argument('notebooks',
-                        help='One or more notebook(s) '
-                             'to be converted, with extension among'
-                             "'{}'".format("', '".join(NOTEBOOK_EXTENSIONS)),
+                        help='One or more notebook(s) to be converted',
                         nargs='+')
-    parser.add_argument('to',
-                        help="Destination notebook 'notebook.md', "
-                             "extension '.md', "
-                             "or format 'md' (for on screen display)")
+    parser.add_argument('-o', '--output',
+                        help='Destination file. Defaults to original file, '
+                             'with extension changed to destination format. '
+                             "Use '-' for printing the notebook on stdout.")
     parser.add_argument('--update', action='store_true',
                         help='Preserve outputs of .ipynb destination '
                              '(when file exists and inputs match)')
     parser.add_argument('--test', dest='test', action='store_true',
                         help='Test that notebook is stable under '
                              'round trip conversion')
-    return parser.parse_args(args)
+    args = parser.parse_args(args)
+
+    if not args.to:
+        if args.output and args.output != '-':
+            _, ext = os.path.splitext(args.output)
+        else:
+            raise TypeError('Please specificy either --to or --output')
+    elif args.to in NOTEBOOK_EXTENSIONS:
+        pass
+    elif '.' + args.to in NOTEBOOK_EXTENSIONS:
+        ext = '.' + args.to
+    else:
+        ext = {'notebook': '.ipynb',
+               'python': '.py',
+               'julia': '.jl',
+               'r': '.R',
+               'markdown': '.md',
+               'rmarkdown': '.Rmd'}[args.to]
+
+    if ext not in NOTEBOOK_EXTENSIONS:
+        raise TypeError('Output extensions should be in {}'
+                        .format(", ".join(NOTEBOOK_EXTENSIONS)))
+
+    if args.update and ext != '.ipynb':
+        raise ValueError('--update works exclusively with --to notebook')
+
+    args.to = ext
+
+    return args
 
 
 def jupytext(args=None):
     """Entry point for the jupytext script"""
-    args = cli_jupytext(args)
-    convert_notebook_files(nb_files=args.notebooks, nb_dest=args.to,
+    try:
+        args = cli_jupytext(args)
+    except (ValueError, TypeError) as err:
+        print('jupytext: error: ' + str(err))
+        exit(1)
+    convert_notebook_files(nb_files=args.notebooks,
+                           ext=args.to,
+                           output=args.output,
                            test_round_trip=args.test,
                            preserve_outputs=args.update)
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -16,13 +16,13 @@ file_format_version.FILE_FORMAT_VERSION = {}
                          list_all_notebooks('.ipynb') +
                          list_all_notebooks('.Rmd'))
 def test_cli_single_file(nb_file):
-    assert cli_jupytext([nb_file] + ['py']).notebooks == [nb_file]
+    assert cli_jupytext([nb_file] + ['--to', 'py']).notebooks == [nb_file]
 
 
 @pytest.mark.parametrize('nb_files', [list_all_notebooks('.ipynb') +
                                       list_all_notebooks('.Rmd')])
 def test_cli_multiple_files(nb_files):
-    assert cli_jupytext(nb_files + ['py']).notebooks == nb_files
+    assert cli_jupytext(nb_files + ['--to', 'py']).notebooks == nb_files
 
 
 @pytest.mark.parametrize('nb_file',
@@ -33,7 +33,7 @@ def test_convert_single_file_in_place(nb_file, tmpdir):
     nb_other = base + '.py'
 
     copyfile(nb_file, nb_org)
-    convert_notebook_files([nb_org], nb_dest='.py')
+    convert_notebook_files([nb_org], ext='.py')
 
     nb1 = readf(nb_org)
     nb2 = readf(nb_other)
@@ -47,7 +47,7 @@ def test_convert_single_file_in_place(nb_file, tmpdir):
 def test_convert_single_file(nb_file, capsys):
     nb1 = readf(nb_file)
     pynb = writes(nb1, ext='.py')
-    convert_notebook_files([nb_file], nb_dest='py')
+    convert_notebook_files([nb_file], ext='.py', output='-')
 
     out, err = capsys.readouterr()
     assert err == ''
@@ -68,7 +68,7 @@ def test_convert_multiple_file(nb_files, tmpdir):
         nb_orgs.append(nb_org)
         nb_others.append(nb_other)
 
-    convert_notebook_files(nb_orgs, '.py')
+    convert_notebook_files(nb_orgs, ext='.py')
 
     for nb_org, nb_other in zip(nb_orgs, nb_others):
         nb1 = readf(nb_org)
@@ -78,17 +78,38 @@ def test_convert_multiple_file(nb_files, tmpdir):
 
 def test_error_not_notebook_ext_input(nb_file='notebook.ext'):
     with pytest.raises(TypeError):
-        convert_notebook_files([nb_file], '.py')
+        convert_notebook_files([nb_file], ext='.py')
 
 
-def test_error_not_notebook_ext_dest(nb_file=list_all_notebooks('.ipynb')[0]):
+def test_error_not_notebook_ext_dest1(nb_file=list_all_notebooks('.ipynb')[0]):
     with pytest.raises(TypeError):
-        convert_notebook_files([nb_file], '.ext')
+        convert_notebook_files([nb_file], ext='.ext')
+
+
+def test_error_not_notebook_ext_output(
+        nb_file=list_all_notebooks('.ipynb')[0]):
+    with pytest.raises(TypeError):
+        cli_jupytext([nb_file, '-o', 'not.ext'])
+
+
+def test_error_no_ext(nb_file=list_all_notebooks('.ipynb')[0]):
+    with pytest.raises(TypeError):
+        cli_jupytext([nb_file])
+
+
+def test_error_not_same_ext(nb_file=list_all_notebooks('.ipynb')[0]):
+    with pytest.raises(TypeError):
+        convert_notebook_files([nb_file], ext='.py', output='not.ext')
+
+
+def test_error_update_not_ipynb(nb_file=list_all_notebooks('.ipynb')[0]):
+    with pytest.raises(ValueError):
+        cli_jupytext([nb_file, '--to', 'py', '--update'])
 
 
 def test_error_multiple_input(nb_files=list_all_notebooks('.ipynb')):
     with pytest.raises(ValueError):
-        convert_notebook_files(nb_files, 'notebook.py')
+        convert_notebook_files(nb_files, ext='.py', output='notebook.py')
 
 
 def test_combine_same_version_ok(tmpdir):
@@ -112,11 +133,11 @@ def test_combine_same_version_ok(tmpdir):
     with mock.patch('jupytext.file_format_version.FILE_FORMAT_VERSION',
                     {'.py': '1.0'}):
         # to jupyter notebook
-        jupytext(args=[tmp_nbpy, '.ipynb', '--update'])
+        jupytext(args=[tmp_nbpy, '--to', 'ipynb', '--update'])
         # test round trip
-        jupytext(args=[tmp_nbpy, '.ipynb', '--test'])
+        jupytext(args=[tmp_nbpy, '--to', 'notebook', '--test'])
         # test ipynb to rmd
-        jupytext(args=[tmp_ipynb, '.Rmd'])
+        jupytext(args=[tmp_ipynb, '--to', 'rmarkdown'])
 
     nb = readf(tmp_ipynb)
     cells = nb['cells']
@@ -151,4 +172,4 @@ def test_combine_lower_version_raises(tmpdir):
     with pytest.raises(ValueError):
         with mock.patch('jupytext.file_format_version.FILE_FORMAT_VERSION',
                         {'.py': '1.0'}):
-            jupytext(args=[tmp_nbpy, '.ipynb', '--update'])
+            jupytext(args=[tmp_nbpy, '--to', 'ipynb', '--update'])
