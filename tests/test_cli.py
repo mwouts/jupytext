e015361baec178cd712eabd6ad6df19fcb40ce0c
Marc Wouts <marc.wouts@gmail.com>
2018-09-07 02:09:06 +0200

    min_file_format_version required for that test

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -173,4 +173,7 @@ def test_combine_lower_version_raises(tmpdir):
     with pytest.raises(SystemExit):
         with mock.patch('jupytext.file_format_version.FILE_FORMAT_VERSION',
                         {'.py': '1.0'}):
-            jupytext(args=[tmp_nbpy, '--to', 'ipynb', '--update'])
+            with mock.patch(
+                    'jupytext.file_format_version.MIN_FILE_FORMAT_VERSION',
+                            {'.py': '1.0'}):
+                jupytext(args=[tmp_nbpy, '--to', 'ipynb', '--update'])
