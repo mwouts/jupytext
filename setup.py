21ae40f44785c5427d2347db285465f8b57c3f86
Marc Wouts <marc.wouts@gmail.com>
2018-09-11 00:05:58 +0200

    Python 3.7 now covered thanks to #68

--- setup.py
+++ setup.py
@@ -35,5 +35,6 @@ setup(
                  'Programming Language :: Python :: 3',
                  'Programming Language :: Python :: 3.4',
                  'Programming Language :: Python :: 3.5',
-                 'Programming Language :: Python :: 3.6')
+                 'Programming Language :: Python :: 3.6',
+                 'Programming Language :: Python :: 3.7')
 )
