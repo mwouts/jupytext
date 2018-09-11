025ee4c1e8fe52e132244e5e955bd39b6912ef68
Marc Wouts <marc.wouts@gmail.com>
2018-09-11 12:10:24 +0200

    Read README as unicode for #71

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from os import path
 from setuptools import setup, find_packages
 
 this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md')) as f:
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
