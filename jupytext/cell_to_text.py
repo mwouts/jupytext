e42bba0b3d72e784a67ea3ac98e0392fb5b99a22
Marc Wouts <marc.wouts@gmail.com>
2018-09-15 01:19:41 +0200

    Use inheritance rather than ifs

--- jupytext/cell_to_text.py
+++ jupytext/cell_to_text.py
@@ -10,11 +10,7 @@ from .magics import escape_magic, escape_code_start
 
 
 def cell_source(cell):
-    """
-    Return the source of the current cell, as an array of lines
-    :param cell:
-    :return:
-    """
+    """Return the source of the current cell, as an array of lines"""
     source = cell.source
     if source == '':
         return ['']
@@ -23,99 +19,16 @@ def cell_source(cell):
     return source.splitlines()
 
 
-def code_to_rmd(source, metadata, language):
-    """
-    Represent a code cell with given source and metadata as a rmd cell
-    :param source:
-    :param metadata:
-    :param language:
-    :return:
-    """
-    lines = []
-    if not is_active('Rmd', metadata):
-        metadata['eval'] = False
-    options = metadata_to_rmd_options(language, metadata)
-    lines.append('```{{{}}}'.format(options))
-    lines.extend(source)
-    lines.append('```')
-    return lines
-
-
-def code_to_md(source, metadata, language):
-    """
-    Represent a code cell with given source and metadata as a md cell
-    :param source:
-    :param metadata:
-    :param language:
-    :return:
-    """
-    options = []
-    if language:
-        options.append(language)
-    if 'name' in metadata:
-        options.append(metadata['name'])
-
-    return ['```{}'.format(' '.join(options))] + source + ['```']
-
-
-def code_to_r(source, metadata):
-    """
-    Represent a code cell with given source and metadata as a R cell
-    :param source:
-    :param metadata:
-    :return:
-    """
-    lines = []
-    if not is_active('R', metadata):
-        metadata['eval'] = False
-    options = metadata_to_rmd_options(None, metadata)
-    if options:
-        lines.append('#+ {}'.format(options))
-    lines.extend(source)
-    return lines
-
-
-def code_to_py(source, metadata):
-    """
-    Represent a code cell with given source and metadata as a python cell
-    """
-    lines = []
-    if not metadata:
-        return source
-
-    endofcell = metadata['endofcell']
-    if endofcell == '-':
-        del metadata['endofcell']
-    options = metadata_to_json_options(metadata)
-    lines.append('# + {}'.format(options))
-    lines.extend(source)
-    lines.append('# {}'.format(endofcell))
-    return lines
-
-
-def py_endofcell_marker(source):
-    """Issues #31 #38:  does the cell contain a blank line? In that case
-    we add an end-of-cell marker"""
-    endofcell = '-'
-    while True:
-        endofcell_re = re.compile(r'^#( )' + endofcell + r'\s*$')
-        if list(filter(endofcell_re.match, source)):
-            endofcell = endofcell + '-'
-        else:
-            return endofcell
-
-
-class CellExporter():
+class BaseCellExporter:
     """A class that represent a notebook cell as text"""
 
     def __init__(self, cell, default_language, ext):
         self.ext = ext
-        self.prefix = '' if ext in ['.Rmd', '.md'] \
-            else "#'" if ext == '.R' else '#'
         self.cell_type = cell.cell_type
         self.source = cell_source(cell)
         self.metadata = filter_metadata(cell.metadata)
         self.language = cell_language(self.source) or default_language
+        self.default_language = default_language
 
         # how many blank lines before next cell
         self.lines_to_next_cell = cell.metadata.get('lines_to_next_cell', 1)
@@ -145,13 +58,13 @@ class CellExporter():
             return self.code_to_text()
 
         source = copy(self.source)
-        if self.ext in ['.Rmd', '.md']:
+        if not self.prefix:
             escape_code_start(source, self.ext, None)
         return self.markdown_escape(source)
 
     def markdown_escape(self, source):
         """Escape the given source, for a markdown cell"""
-        if self.ext in ['.Rmd', '.md']:
+        if not self.prefix:
             return source
         return [self.prefix + ' ' + line if line else self.prefix
                 for line in source]
@@ -168,39 +81,134 @@ class CellExporter():
 
         return False
 
+
+class MarkdownCellExporter(BaseCellExporter):
+    """A class that represent a notebook cell as Markdown"""
+    prefix = ''
+
+    def code_to_text(self):
+        """Return the text representation of a code cell"""
+        source = copy(self.source)
+        escape_code_start(source, self.ext, self.language)
+
+        options = []
+        if self.cell_type == 'code' and self.language:
+            options.append(self.language)
+        if 'name' in self.metadata:
+            options.append(self.metadata['name'])
+
+        return ['```{}'.format(' '.join(options))] + source + ['```']
+
+
+class RMarkdownCellExporter(BaseCellExporter):
+    """A class that represent a notebook cell as Markdown"""
+    prefix = ''
+
     def code_to_text(self):
         """Return the text representation of a code cell"""
         active = is_active(self.ext, self.metadata)
-        if self.ext in ['.R', '.py', '.jl']:
-            if active and self.language != (
-                    'R' if self.ext == '.R' else
-                    'python' if self.ext == '.py' else 'julia'):
-                active = False
-                self.metadata['active'] = 'ipynb'
-                self.metadata['language'] = self.language
+        source = copy(self.source)
+        escape_code_start(source, self.ext, self.language)
+
+        if active:
+            escape_magic(source, self.language)
+
+        lines = []
+        if not is_active('Rmd', self.metadata):
+            self.metadata['eval'] = False
+        options = metadata_to_rmd_options(self.language, self.metadata)
+        lines.append('```{{{}}}'.format(options))
+        lines.extend(source)
+        lines.append('```')
+        return lines
+
+
+class LightScriptCellExporter(BaseCellExporter):
+    """A class that represent a notebook cell as a Python or Julia script"""
+    prefix = '#'
+
+    def endofcell_marker(self, source):
+        """Issues #31 #38:  does the cell contain a blank line? In that case
+        we add an end-of-cell marker"""
+        endofcell = '-'
+        while True:
+            endofcell_re = re.compile(r'^#( )' + endofcell + r'\s*$')
+            if list(filter(endofcell_re.match, source)):
+                endofcell = endofcell + '-'
+            else:
+                return endofcell
+
+    def code_to_text(self):
+        """Return the text representation of a code cell"""
+        active = is_active(self.ext, self.metadata)
+        if active and self.language != (
+                'python' if self.ext == '.py' else 'julia'):
+            active = False
+            self.metadata['active'] = 'ipynb'
+            self.metadata['language'] = self.language
 
         source = copy(self.source)
         escape_code_start(source, self.ext, self.language)
 
-        if active and self.ext != '.md':
+        if active:
             escape_magic(source, self.language)
+        else:
+            source = ['# ' + line if line else '#' for line in source]
 
-        if self.ext == '.Rmd':
-            return code_to_rmd(source, self.metadata, self.language)
+        if self.explicit_start_marker(source):
+            self.metadata['endofcell'] = self.endofcell_marker(source)
 
-        if self.ext == '.md':
-            return code_to_md(
-                source, self.metadata,
-                self.language if self.cell_type == 'code' else None)
+        if not self.metadata:
+            return source
+
+        lines = []
+        endofcell = self.metadata['endofcell']
+        if endofcell == '-':
+            del self.metadata['endofcell']
+        options = metadata_to_json_options(self.metadata)
+        lines.append('# + {}'.format(options))
+        lines.extend(source)
+        lines.append('# {}'.format(endofcell))
+        return lines
+
+
+class RScriptCellExporter(BaseCellExporter):
+    """A class that represent a notebook cell as a R script"""
+    prefix = "#'"
+
+    def code_to_text(self):
+        """Return the text representation of a code cell"""
+        active = is_active(self.ext, self.metadata)
+        if active and self.language != 'R':
+            active = False
+            self.metadata['active'] = 'ipynb'
+            self.metadata['language'] = self.language
+
+        source = copy(self.source)
+        escape_code_start(source, self.ext, self.language)
+
+        if active:
+            escape_magic(source, self.language)
 
         if not active:
             source = ['# ' + line if line else '#' for line in source]
 
-        if self.ext == '.R':
-            return code_to_r(source, self.metadata)
+        lines = []
+        if not is_active('R', self.metadata):
+            self.metadata['eval'] = False
+        options = metadata_to_rmd_options(None, self.metadata)
+        if options:
+            lines.append('#+ {}'.format(options))
+        lines.extend(source)
+        return lines
 
-        # py, jl
-        if self.explicit_start_marker(source):
-            self.metadata['endofcell'] = py_endofcell_marker(source)
 
-        return code_to_py(source, self.metadata)
+def CellExporter(cell, default_language, ext):
+    if ext == '.md':
+        return MarkdownCellExporter(cell, default_language, ext)
+    if ext == '.Rmd':
+        return RMarkdownCellExporter(cell, default_language, ext)
+    if ext == '.R':
+        return RScriptCellExporter(cell, default_language, ext)
+
+    return LightScriptCellExporter(cell, default_language, ext)
