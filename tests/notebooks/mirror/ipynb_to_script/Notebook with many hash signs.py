e2a0ea8a138560911db14b8f35ad2384cf453c7d
Marc Wouts <marc.wouts@gmail.com>
2018-09-23 23:18:07 +0200

    Additional test to make sure formats are robust

---
+++ tests/notebooks/mirror/ipynb_to_script/Notebook with many hash signs.py
@@ -0,0 +1,38 @@
+# ---
+# jupyter:
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+#   language_info:
+#     codemirror_mode:
+#       name: ipython
+#       version: 3
+#     file_extension: .py
+#     mimetype: text/x-python
+#     name: python
+#     nbconvert_exporter: python
+#     pygments_lexer: ipython3
+#     version: 3.6.6
+# ---
+
+# ##################################################################
+# This is a notebook that contains many hash signs.
+# Hopefully its python representation is not recognized as a Sphinx Gallery script...
+# ##################################################################
+
+# +
+some = 1
+code = 2
+some+code
+
+##################################################################
+# A comment
+##################################################################
+# Another comment
+# -
+
+# ##################################################################
+# This is a notebook that contains many hash signs.
+# Hopefully its python representation is not recognized as a Sphinx Gallery script...
+# ##################################################################
