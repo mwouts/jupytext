6876d7c61652c8ba87c752c9d0a34268efea4bff
Marc Wouts <marc.wouts@gmail.com>
2018-10-08 01:35:14 +0200

    One spyder cell is enought to activate percent format #59

--- tests/test_formats.py
+++ tests/test_formats.py
@@ -19,3 +19,18 @@ def test_guess_format_percent(nb_file):
 def test_guess_format_sphinx(nb_file):
     with open(nb_file) as stream:
         assert guess_format(stream.read(), ext='.py') == 'sphinx'
+
+
+def test_script_with_magics_not_percent(script="""# %%time
+1 + 2"""):
+    assert guess_format(script, '.py') == 'light'
+
+
+def test_script_with_spyder_cell_is_percent(script="""#%%
+1 + 2"""):
+    assert guess_format(script, '.py') == 'percent'
+
+
+def test_script_with_spyder_cell_with_name_is_percent(script="""#%% cell name
+1 + 2"""):
+    assert guess_format(script, '.py') == 'percent'
