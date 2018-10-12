ecb9587ebfc0479a96f8c4c08ded93cccf3f019d
Marc Wouts <marc.wouts@gmail.com>
2018-10-12 07:12:04 +0200

    reflowing code

--- jupytext/compare.py
+++ jupytext/compare.py
@@ -10,12 +10,13 @@ def filtered_cell(cell, preserve_outputs):
     """Cell type, metadata and source from given cell"""
     filtered = {'cell_type': cell.cell_type,
                 'source': cell.source,
-                'metadata': {key: cell.metadata[key] for key in cell.metadata
-                             if key not in _IGNORE_METADATA}}
+                'metadata': {key: cell.metadata[key] for key in cell.metadata if key not in _IGNORE_METADATA}}
+
     if preserve_outputs:
         for key in ['execution_count', 'outputs']:
             if key in cell:
                 filtered[key] = cell[key]
+
     return filtered
 
 
@@ -24,8 +25,10 @@ def filtered_notebook_metadata(notebook):
     return {key: notebook.metadata[key] for key in notebook.metadata if key != 'jupytext'}
 
 
-def compare_notebooks(notebook_expected, notebook_actual,
-                      allow_split_markdown=False, test_outputs=False):
+def compare_notebooks(notebook_expected,
+                      notebook_actual,
+                      allow_split_markdown=False,
+                      test_outputs=False):
     """Compare the two notebooks, and raise with a meaningful message
     that explains one difference, if any"""
 
@@ -35,8 +38,7 @@ def compare_notebooks(notebook_expected, notebook_actual,
         try:
             test_cell = next(test_cell_iter)
         except StopIteration:
-            compare(filtered_cell(ref_cell,
-                                  preserve_outputs=test_outputs), None)
+            compare(filtered_cell(ref_cell, preserve_outputs=test_outputs), None)
 
         if allow_split_markdown and ref_cell.cell_type == 'markdown':
             ref_lines = [line for line in ref_cell.source.splitlines()
@@ -47,9 +49,7 @@ def compare_notebooks(notebook_expected, notebook_actual,
                 compare(ref_cell.cell_type, test_cell.cell_type)
                 compare(ref_cell.metadata, test_cell.metadata)
 
-                test_lines.extend([line for line in
-                                   test_cell.source.splitlines()
-                                   if line != ''])
+                test_lines.extend([line for line in test_cell.source.splitlines() if line != ''])
 
                 if len(test_lines) >= len(ref_lines):
                     break
@@ -77,12 +77,14 @@ def test_round_trip_conversion(notebook, ext, format_name, test_outputs):
     text = writes(notebook, ext=ext, format_name=format_name)
     round_trip = reads(text, ext=ext, format_name=format_name)
 
-    compare_notebooks(notebook, round_trip,
+    compare_notebooks(notebook,
+                      round_trip,
                       allow_split_markdown=(ext in ['.Rmd', '.md']),
                       test_outputs=False)
 
     if test_outputs:
         combine_inputs_with_outputs(round_trip, notebook)
-        compare_notebooks(notebook, round_trip,
+        compare_notebooks(notebook,
+                          round_trip,
                           allow_split_markdown=(ext in ['.Rmd', '.md']),
                           test_outputs=True)
