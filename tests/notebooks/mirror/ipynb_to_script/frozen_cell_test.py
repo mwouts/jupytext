e3295d96f9254d6eaa4ddc7ea2e42cf2ec22b335
Marc Wouts <marc.wouts@gmail.com>
2018-10-18 22:57:39 +0200

    New version of mirror notebook #101

---
+++ tests/notebooks/mirror/ipynb_to_script/frozen_cell_test.py
@@ -0,0 +1,25 @@
+# ---
+# jupyter:
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+#   language_info:
+#     codemirror_mode:
+#       name: ipython
+#       version: 3
+#     file_extension: .py
+#     mimetype: text/x-python
+#     name: python
+#     nbconvert_exporter: python
+#     pygments_lexer: ipython3
+#     version: 3.7.0
+# ---
+
+# + {"ExecuteTime": {"end_time": "2018-10-17T10:31:56.160823Z", "start_time": "2018-10-17T10:31:56.157308Z"}}
+# This is an unfrozen cell. Works as usual.
+print("I'm a regular cell so I run and print!")
+
+# + {"deletable": false, "editable": false, "run_control": {"frozen": true}}
+# # This is an frozen cell
+# print("I'm frozen so Im not executed :(")
