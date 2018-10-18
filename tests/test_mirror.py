a0b29d74cecbcb024a85901360d4641912831f8f
Marc Wouts <marc.wouts@gmail.com>
2018-10-18 23:17:27 +0200

    Frozen cells are not supported in plain markdown #101

--- tests/test_mirror.py
+++ tests/test_mirror.py
@@ -164,6 +164,6 @@ def test_ipynb_to_Rmd(nb_file):
     assert_conversion_same_as_mirror(nb_file, '.Rmd', 'ipynb_to_Rmd')
 
 
-@pytest.mark.parametrize('nb_file', list_notebooks('ipynb', skip='66'))
+@pytest.mark.parametrize('nb_file', list_notebooks('ipynb', skip='(66|frozen)'))
 def test_ipynb_to_md(nb_file):
     assert_conversion_same_as_mirror(nb_file, '.md', 'ipynb_to_md')
