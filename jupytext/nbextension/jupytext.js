a09f9b201f7195b8ac3b6bd0616724d0c40292f0
Marc Wouts <marc.wouts@gmail.com>
2018-11-30 01:12:34 +0100

    Initial layout for the config dialog

--- jupytext/nbextension/jupytext.js
+++ jupytext/nbextension/jupytext.js
@@ -7,7 +7,8 @@ Test the extension in a Jupyter notebook by adding two cells:
 ```python
 # 1. Install the extension
 import notebook.nbextensions
-notebook.nbextensions.install_nbextension('.../jupytext/nbextension/jupytext.js', user=True)
+notebook.nbextensions.install_nbextension('jupytext.js', user=True)
+# Or, more convenient for developing: replace the installed file with a symbolic link to this one
 ```
 
 ```python
@@ -23,96 +24,128 @@ define([
     'jquery',
     'base/js/namespace',
     'base/js/events',
-], function(
+], function (
     requirejs,
     $,
     Jupyter,
     events
 ) {
-    "use strict";
-    
-    var show_notebook_settings_dialog = function () {
+        "use strict";
 
-        var modal = $('<div class="modal fade" role="dialog"/>');
-        var dialog_content = $("<div/>")
-            .addClass("modal-content")
-            .appendTo($('<div class="modal-dialog">').appendTo(modal));
-        $('<div class="modal-header">')
-            .append('<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>')
-            .append('<h4 class="modal-title">Jupytext settings</h4>')
-            .on('mousedown', function() { $('.modal').draggable({handle: '.modal-header'});})
-            .appendTo(dialog_content);
-        $('<div>')
-            .addClass('modal-body')
-            .append([
-                $('<div>').text(JSON.stringify(Jupyter.notebook.metadata.jupytext)),
-            ])
-            .append('<button type="button" id="jupytext-enable">new button</button>')
-            .append(' <select id="jupytext-dropdownList"> \
-                        <option value="1">Option 1</option>  \
-                        <option value="2">Option 2</option>  \
-                      </select>')
-            .appendTo(dialog_content);
-        $('<div class="modal-footer">')
-            .append('<button class="btn btn-default btn-sm btn-primary" data-dismiss="modal">Ok</button>')
-            .appendTo(dialog_content);
-        // focus button on open
-        modal.on('shown.bs.modal', function () {
-            setTimeout(function () {
-                dialog_content.find('.modal-footer button').last().focus();
-                $("#jupytext-enable").on("click", function(){
-                  console.log("jupytext-enable clicked");
-                });
-                // see this tutorial for selected value https://www.codexworld.com/how-to/get-text-value-of-selected-option-using-jquery/
-                $('#jupytext-dropdownList').on('change',function(){
-                  var optionValue = $(this).val();
-                  alert(optionValue)
-                  //var optionText = $('#dropdownList option[value="'+optionValue+'"]').text();
-                  // var optionText = $("#dropdownList option:selected").text();
-                  // alert("Selected Option Text: "+optionText);
-                });
-            }, 0);
-        });
+        var show_notebook_settings_dialog = function () {
 
-        return modal.modal({backdrop: 'static'});
-    };
+            var modal = $('<div class="modal fade" role="dialog"/>');
+            var dialog_content = $("<div/>")
+                .addClass("modal-content")
+                .appendTo($('<div class="modal-dialog">').appendTo(modal));
+            $('<div class="modal-header">')
+                .append('<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>')
+                .append('<h3 class="modal-title">Jupytext configuration for the current notebook</h3>')
+                .on('mousedown', function () { $('.modal').draggable({ handle: '.modal-header' }); })
+                .appendTo(dialog_content);
+            $('<div>')
+                .addClass('modal-body')
+                .append('<h3>Notebook formats</h3>')
+                .append(`<input type="checkbox" id="jupytext-formats-ipynb"/> <label>Jupyter notebook (.ipynb)</label><br>
+                         <input type="checkbox" id="jupytext-formats-script"/> <label>Python script (.py)</label> with format
+                            <select id="jupytext-formats-script-format">
+                                <option value="light">light</option>
+                                <option value="percent">percent</option>
+                                <option value="spin">spin (.R only)</option>
+                                <option value="sphinx">sphinx (.py only)</option>
+                            </select><br>
+                    <input type="checkbox" id="jupytext-formats-markdown"/> <label>Markdown document (.md)</label> <br>
+                    <input type="checkbox" id="jupytext-formats-Rmarkdown"/> <label>R Markdown notebook (.Rmd)</label>`)
+                // TODO: Replace 'Python script (.py)' above with a value that is taken from Jupyter.notebook.metadata.language_info
+                // TODO (later): spin/sphinx only available for .R or .py formats
+                // TODO: Set the initial values above from Jupyter.notebook.metadata.jupytext.formats
+                // TODO: Create or update Jupyter.notebook.metadata.jupytext.formats to match the selected values
 
-    var jupytext_button = function() {
-        if (!Jupyter.toolbar) {
-            $([Jupyter.events]).on("app_initialized.NotebookApp", jupytext_button);
-            return;
+                .append(`<h3>Jupyter magic commands</h3>
+                    <input type="radio" name="jupytext-comment-magics" value="default" checked="true">Default (commented in scripts and Rmd)
+                    <input type="radio" name="jupytext-comment-magics" value="true">Commented
+                    <input type="radio" name="jupytext-comment-magics" value="false">Not commented`)
+                // TODO: Set the initial values above from Jupyter.notebook.metadata.jupytext.comment_magics
+                // TODO: Create, update or delete Jupyter.notebook.metadata.jupytext.comment_magics to match the selected value
+
+                .append('<h3>Metadata filters</h3>')
+                .append('<h4>Notebook metadata</h4>')
+                // TODO (Later): Choose between default/all/none/checklist filled with keys of Jupyter.notebook.metadata
+                .append(`<input type="radio" name="jupytext-metadata-filter-notebook" value="default" checked="true">Default
+                <input type="radio" name="jupytext-metadata-filter-notebook" value="none">None
+                <input type="radio" name="jupytext-metadata-filter-notebook" value="all">All`)
+                .append('<h4>Cell metadata</h4>')
+                // TODO (Later): Choose between default/all/none/checklist filled with keys of metatadata in one of Jupyter.notebook.cells
+                .append(`<input type="radio" name="jupytext-metadata-filter-cell" value="default" checked="true">Default:
+                <input type="radio" name="jupytext-metadata-filter-cell" value="none">None
+                <input type="radio" name="jupytext-metadata-filter-cell" value="all">All`)
+
+                .append('<h3>Debug: display jupytext metadata</h3>')
+                .append($('<div id="jupytext-metadata"/>').html(JSON.stringify(Jupyter.notebook.metadata.jupytext)))
+
+                .appendTo(dialog_content);
+            $('<div class="modal-footer">')
+                .append('<button class="btn btn-default btn-sm btn-primary" data-dismiss="modal">Ok</button>')
+                .appendTo(dialog_content);
+
+
+            // focus button on open
+            modal.on('shown.bs.modal', function () {
+                setTimeout(function () {
+                    dialog_content.find('.modal-footer button').last().focus();
+                    $("#jupytext-formats-ipynb").on("change", function () {
+                        console.log("jupytext-formats-ipynb changed");
+                    });
+                    // see this tutorial for selected value https://www.codexworld.com/how-to/get-text-value-of-selected-option-using-jquery/
+                    $('#jupytext-dropdownList').on('change', function () {
+                        var optionValue = $(this).val();
+                        alert(optionValue)
+                        //var optionText = $('#dropdownList option[value="'+optionValue+'"]').text();
+                        // var optionText = $("#dropdownList option:selected").text();
+                        // alert("Selected Option Text: "+optionText);
+                    });
+                }, 0);
+            });
+
+            return modal.modal({ backdrop: 'static' });
+        };
+
+        var jupytext_button = function () {
+            if (!Jupyter.toolbar) {
+                $([Jupyter.events]).on("app_initialized.NotebookApp", jupytext_button);
+                return;
+            }
+            if ($("#jupytext_button").length === 0) {
+                $(IPython.toolbar.add_buttons_group([
+                    Jupyter.keyboard_manager.actions.register({
+                        'help': 'Jupytext',
+                        'icon': 'fa-clone', // fa-wrench fa-clone fa-file-alt fa-file-export fa-object-group fa-readme
+                        'handler': show_notebook_settings_dialog,
+                    }, 'jupytext-button', 'jupytext')
+                ])).find('.btn').attr('id', 'jupytext_button');
+            }
+        };
+
+        var jupytext_init = function () {
+            jupytext_button();
         }
-        if ($("#jupytext_button").length === 0) {
-            Jupyter.toolbar.add_buttons_group([{
-                    'label' : 'Jupytext',
-                    'help'  : 'Jupytext settings',
-                    'icon'  : 'fa-wrench',
-                    'callback': show_notebook_settings_dialog,
-                },
-            ]);
-        }
-    };
 
-    var jupytext_init = function() {
-        jupytext_button();
-    }
-
-    var load_ipython_extension = function() {
-        // Wait for the notebook to be fully loaded
-        if (Jupyter.notebook !== undefined && Jupyter.notebook._fully_loaded) {
-            // this tests if the notebook is fully loaded
-            console.log("[jupytext] Notebook fully loaded -- jupytext extension initialized ")
-            jupytext_init();
-        } else {
-            console.log("[jupytext] Waiting for notebook availability")
-            events.on("notebook_loaded.Notebook", function() {
-                console.log("[jupytext] jupytext initialized (via notebook_loaded)")
+        var load_ipython_extension = function () {
+            // Wait for the notebook to be fully loaded
+            if (Jupyter.notebook !== undefined && Jupyter.notebook._fully_loaded) {
+                // this tests if the notebook is fully loaded
+                console.log("[jupytext] Notebook fully loaded -- jupytext extension initialized ")
                 jupytext_init();
-            })
-        }
-    };
+            } else {
+                console.log("[jupytext] Waiting for notebook availability")
+                events.on("notebook_loaded.Notebook", function () {
+                    console.log("[jupytext] jupytext initialized (via notebook_loaded)")
+                    jupytext_init();
+                })
+            }
+        };
 
-    return {
-        load_ipython_extension: load_ipython_extension
-    };
-});
+        return {
+            load_ipython_extension: load_ipython_extension
+        };
+    });
