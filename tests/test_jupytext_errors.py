0e28b96bccf89df8fe2a6b2b86e1c8d067084989
Marc Wouts <marc.wouts@gmail.com>
2018-12-17 00:05:37 +0100

    use nb_file argument

--- tests/test_jupytext_errors.py
+++ tests/test_jupytext_errors.py
@@ -4,13 +4,13 @@ from nbformat.v4.nbbase import new_notebook
 
 
 def test_read_wrong_ext(tmpdir, nb_file='notebook.ext'):
-    nb_file = tmpdir.join('nb_file')
+    nb_file = tmpdir.join(nb_file)
     nb_file.write('{}')
     with pytest.raises(TypeError):
         jupytext.readf(str(nb_file))
 
 
 def test_write_wrong_ext(tmpdir, nb_file='notebook.ext'):
-    nb_file = str(tmpdir.join('nb_file'))
+    nb_file = str(tmpdir.join(nb_file))
     with pytest.raises(TypeError):
         jupytext.writef(new_notebook(), nb_file)
