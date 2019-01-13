c69f52092a2fb2e143d1479692ac063ae9b8ee62
Marc Wouts <marc.wouts@gmail.com>
2019-01-14 00:23:44 +0100

    Removed obsolete documentation

--- jupytext/__init__.py
+++ jupytext/__init__.py
@@ -1,14 +1,4 @@
-"""R markdown notebook format for Jupyter
-
-Use this module to read or write Jupyter notebooks as Markdown documents,
-R markdown documents, Python or R scripts (methods 'read', 'reads', 'write',
-'writes')
-
-Use the TextFileContentsManager to open Text notebooks in Jupyter
-
-Use the 'jupytext' conversion script to convert Jupyter notebooks from/to
-R Markdown notebooks.
-"""
+"""Read and write Jupyter notebooks as text files"""
 
 from .jupytext import readf, writef, writes, reads
 from .formats import NOTEBOOK_EXTENSIONS, guess_format, get_format_implementation
