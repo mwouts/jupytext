f8e657df3efe3ce3ba77983764a07c591c9301e0
Phil Austin <paustin@eos.ubc.ca>
2019-02-09 07:05:01 -0800

    test code

---
+++ test_rc2/python/rc1.py
@@ -0,0 +1,48 @@
+# ---
+# jupyter:
+#   jupytext:
+#     cell_metadata_filter: all
+#     notebook_metadata_filter: all
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.2'
+#       jupytext_version: 1.0.0-rc2
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+#   language_info:
+#     codemirror_mode:
+#       name: ipython
+#       version: 3
+#     file_extension: .py
+#     mimetype: text/x-python
+#     name: python
+#     nbconvert_exporter: python
+#     pygments_lexer: ipython3
+#     version: 3.7.1
+#   toc:
+#     base_numbering: 1
+#     nav_menu: {}
+#     number_sections: true
+#     sideBar: true
+#     skip_h1_title: false
+#     title_cell: Table of Contents
+#     title_sidebar: Contents
+#     toc_cell: true
+#     toc_position: {}
+#     toc_section_display: true
+#     toc_window_display: true
+# ---
+
+# %% [markdown] {"toc": true}
+# <h1>Table of Contents<span class="tocSkip"></span></h1>
+# <div class="toc"><ul class="toc-item"><li><span><a href="#rc1-notebook" data-toc-modified-id="rc1-notebook-1"><span class="toc-item-num">1&nbsp;&nbsp;</span>rc1 notebook</a></span></li></ul></div>
+
+# %% [markdown]
+# # rc1 notebook
+
+# %%
+a=5
+b=6
