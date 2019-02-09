1e49ab5eb0766b92c960e05dc24251cb218c4b3a
Phil Austin <paustin@eos.ubc.ca>
2019-02-09 10:07:20 -0800

    raw checkpoint

---
+++ test_rc2/python/raw_notebook.py
@@ -0,0 +1,11 @@
+# ---
+# jupyter:
+#   jupytext:
+#     cell_metadata_filter: all
+#     notebook_metadata_filter: all
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.2'
+#       jupytext_version: 1.0.0-rc3
+# ---
