cbefb84542d9dfddd0f2fdf8bd0cb2fc89d5b824
Marc Wouts <marc.wouts@gmail.com>
2019-02-12 00:58:21 +0100

    Allow "jupyter nbextension install/enable --py jupytext"

--- jupytext/__init__.py
+++ jupytext/__init__.py
@@ -14,6 +14,21 @@ except ImportError as err:
         def __init__(self):
             raise self.err
 
+def _jupyter_nbextension_paths():
+    """Allows commands like
+     jupyter nbextension install --py jupytext
+     jupyter nbextension enable --py jupytext
+     jupyter labextension install jupyterlab-jupytext"""
+    return [dict(
+        section="notebook",
+        # the path is relative to the `jupytext` directory
+        src="nbextension",
+        # directory in the `nbextension/` namespace
+        dest="jupytext",
+        # _also_ in the `nbextension/` namespace
+        require="jupytext/index")]
+
+
 __all__ = ['readf', 'writef', 'writes', 'reads',
            'NOTEBOOK_EXTENSIONS', 'guess_format', 'get_format_implementation',
            'TextFileContentsManager', '__version__']
