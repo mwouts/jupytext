965ce7ff1135bddd07402428ca233618c0a15cc9
Marc Wouts <marc.wouts@gmail.com>
2019-02-16 01:03:27 +0100

    Jupytext as a nbserver_extension #176

--- jupytext/__init__.py
+++ jupytext/__init__.py
@@ -15,6 +15,22 @@ except ImportError as err:
             raise self.err
 
 
+def load_jupyter_server_extension(app):  # pragma: no cover
+    """Use Jupytext's contents manager"""
+    # The server extension call is too late!
+    # The contents manager was set at NotebookApp.init_configurables
+
+    # Let's change the contents manager class
+    app.contents_manager_class = TextFileContentsManager
+
+    # And rerun the init steps from then to now
+    app.init_configurables()
+    app.init_components()
+    app.init_webapp()
+    app.init_terminals()
+    app.init_signal()
+
+
 def _jupyter_nbextension_paths():  # pragma: no cover
     """Allows commands like
      jupyter nbextension install --py jupytext
--- jupytext/version.py
+++ jupytext/version.py
@@ -1,3 +1,3 @@
 """Jupytext's version number"""
 
-__version__ = '1.0.0-rc4'
+__version__ = '1.0.0-rc4+dev'
