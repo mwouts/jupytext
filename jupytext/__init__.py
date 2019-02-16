2051fe5459fed4f73a1773e65a4756fdeb77f374
Marc Wouts <marc.wouts@gmail.com>
2019-02-16 17:12:43 +0100

    Skip if contents manager is jupytext already
    And log current vs new contents manager
    #176

--- jupytext/__init__.py
+++ jupytext/__init__.py
@@ -17,10 +17,17 @@ except ImportError as err:
 
 def load_jupyter_server_extension(app):  # pragma: no cover
     """Use Jupytext's contents manager"""
+    if app.contents_manager_class == TextFileContentsManager:
+        app.log.info("[Jupytext Server Extension] NotebookApp.contents_manager_class is "
+                     "jupytext.TextFileContentsManager already - OK")
+        return
+
     # The server extension call is too late!
     # The contents manager was set at NotebookApp.init_configurables
 
     # Let's change the contents manager class
+    app.log.info('[Jupytext Server Extension] Changing NotebookApp.contents_manager_class '
+                 'from {} to jupytext.TextFileContentsManager'.format(app.contents_manager_class.__name__))
     app.contents_manager_class = TextFileContentsManager
 
     # And rerun the init steps from then to now
