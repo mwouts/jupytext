75f9011c89531f0961c8d4fab050df9e78711b72
Marc Wouts <marc.wouts@gmail.com>
2019-02-23 01:25:10 +0100

    New notebook with R magic arguments

---
+++ tests/notebooks/mirror/ipynb_to_hydrogen/Notebook_with_more_R_magic_111.py
@@ -0,0 +1,25 @@
+# ---
+# jupyter:
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# %%
+%load_ext rpy2.ipython
+import pandas as pd
+
+df = pd.DataFrame(
+    {
+        "Letter": ["a", "a", "a", "b", "b", "b", "c", "c", "c"],
+        "X": [4, 3, 5, 2, 1, 7, 7, 5, 9],
+        "Y": [0, 4, 3, 6, 7, 10, 11, 9, 13],
+        "Z": [1, 2, 3, 1, 2, 3, 1, 2, 3],
+    }
+)
+
+# %%
+%%R -i df
+library("ggplot2")
+ggplot(data = df) + geom_point(aes(x = X, y = Y, color = Letter, size = Z))
