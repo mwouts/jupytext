18e47b974615e1c078ff8540d4b88f8bd4e71a70
Marc Wouts <marc.wouts@gmail.com>
2019-03-28 22:01:06 +0100

    Use temp files rather than pipes
    #208

--- jupytext/pandoc.py
+++ jupytext/pandoc.py
@@ -1,6 +1,8 @@
 """Jupyter notebook to Markdown and back, using Pandoc"""
 
+import os
 import subprocess
+import tempfile
 import packaging.version
 import nbformat
 
@@ -10,11 +12,21 @@ class PandocError(ChildProcessError):
     pass
 
 
-def pandoc(args, text=''):
+def pandoc(args, filein=None, fileout=None):
     """Execute pandoc with the given arguments"""
-    cmd = [u'pandoc'] + args.split()
-    proc = subprocess.Popen(cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-    out, err = proc.communicate(text.encode('utf-8'))
+    cmd = [u'pandoc']
+
+    if filein:
+        cmd.append(filein)
+
+    if fileout:
+        cmd.append('-o')
+        cmd.append(fileout)
+
+    cmd.extend(args.split())
+
+    proc = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+    out, err = proc.communicate()
     if proc.returncode:
         raise PandocError('pandoc exited with return code {}\n{}'.format(proc.returncode, str(err)))
     return out.decode('utf-8')
@@ -32,12 +44,29 @@ def pandoc_version():
 
 def md_to_notebook(text):
     """Convert a Markdown text to a Jupyter notebook, using Pandoc"""
-    json = pandoc(u'--from markdown --to ipynb --atx-headers', text)
-    return nbformat.reads(json, as_version=4)
+    tmp_file = tempfile.NamedTemporaryFile(delete=False)
+    tmp_file.write(text.encode('utf-8'))
+    tmp_file.close()
+
+    pandoc(u'--from markdown --to ipynb -s --atx-headers --wrap=preserve', tmp_file.name, tmp_file.name)
+
+    with open(tmp_file.name) as opened_file:
+        notebook = nbformat.read(opened_file, as_version=4)
+    os.unlink(tmp_file.name)
+
+    return notebook
 
 
 def notebook_to_md(notebook):
     """Convert a notebook to its Markdown representation, using Pandoc"""
-    json = nbformat.writes(notebook)
-    text = pandoc(u'--from ipynb --to markdown --atx-headers', json)
-    return u'\n'.join(text.splitlines())
+    tmp_file = tempfile.NamedTemporaryFile(delete=False)
+    tmp_file.write(nbformat.writes(notebook).encode('utf-8'))
+    tmp_file.close()
+
+    pandoc(u'--from ipynb --to markdown -s --atx-headers --wrap=preserve', tmp_file.name, tmp_file.name)
+
+    with open(tmp_file.name) as opened_file:
+        text = opened_file.read()
+
+    os.unlink(tmp_file.name)
+    return '\n'.join(text.splitlines())
