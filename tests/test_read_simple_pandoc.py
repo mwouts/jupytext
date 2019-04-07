48889fd825b81e8421d2dd7f077191df72f79e4b
Marc Wouts <marc.wouts@gmail.com>
2019-04-07 02:05:32 +0200

    Python-latex repr for pi is $\\pi$

--- tests/test_read_simple_pandoc.py
+++ tests/test_read_simple_pandoc.py
@@ -43,7 +43,7 @@ bibendum felis dictum sodales.
 def test_pandoc_utf8_in_md(markdown='''::: {.cell .markdown}
 # Utf-8 support
 
-This is the greek letter $\pi$: π
+This is the greek letter $\\pi$: π
 :::'''):
     nb = jupytext.reads(markdown, 'md')
 
@@ -54,7 +54,7 @@ This is the greek letter $\pi$: π
 @requires_pandoc
 def test_pandoc_utf8_in_nb(nb=new_notebook(cells=[new_markdown_cell('''# Utf-8 support
 
-This is the greek letter $\pi$: π''')])):
+This is the greek letter $\\pi$: π''')])):
     markdown = jupytext.writes(nb, 'md:pandoc')
     nb2 = jupytext.reads(markdown, 'md:pandoc')
     nb2.metadata.pop('jupytext')
