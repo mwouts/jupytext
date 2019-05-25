c7fc47c89e50adc94510aec67612c59ddb491ced
Marc Wouts <marc.wouts@gmail.com>
2019-05-25 17:51:34 +0200

    Invalid notebooks may cause a warning, but not a fatal error
    Fixes #234

---
+++ tests/notebooks/mirror/ipynb_to_script/R notebook with invalid cell keys.R
@@ -0,0 +1,12 @@
+# ---
+# jupyter:
+#   kernelspec:
+#     display_name: R
+#     language: R
+#     name: ir
+# ---
+
+# This notebook was created with IRKernel 0.8.12, and is not completely valid, as the code cell below contains an unexpected 'source' entry. This did cause https://github.com/mwouts/jupytext/issues/234. Note that the problem is solved when one upgrades to IRKernel 1.0.0.
+
+library("ggplot2")
+ggplot(mtcars, aes(mpg)) + stat_ecdf()
