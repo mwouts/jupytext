63541686e44d99106317f9c1bbf92529f73127f2
Hatem Hosny <hatemhosny@gmail.com>
2019-06-08 20:19:55 +0200

    add support for javascript and typescript

---
+++ tests/notebooks/mirror/ipynb_to_percent/itypescript.ts
@@ -0,0 +1,37 @@
+// ---
+// jupyter:
+//   kernelspec:
+//     display_name: Typescript 3.5
+//     language: typescript
+//     name: typescript
+// ---
+
+// %%
+let x: number = 5;
+const y: number = 6;
+var z: string = "hi";
+console.log(z);
+x + y;
+
+// %%
+function add(num1: number, num2: number): number {
+    return num1 + num2
+}
+add(x, y);
+
+// %%
+const arrowAdd = (num1: number, num2: number) => num1 + num2;
+arrowAdd(x, y);
+
+// %%
+class User {
+  constructor(private name: string) {
+    this.name = name;
+  }
+  sayHello(): string {
+    return "Hello, I'm " + this.name;
+  }
+}
+let John: User;
+John = new User("John");
+John.sayHello();
---
+++ tests/notebooks/mirror/ipynb_to_script/ijavascript.js
@@ -0,0 +1,55 @@
+// ---
+// jupyter:
+//   kernelspec:
+//     display_name: Javascript (Node.js)
+//     language: javascript
+//     name: javascript
+// ---
+
+// ## A notebook that uses IJavascript kernel
+
+let x = 5;
+const y = 6;
+var z = 10;
+
+x + y;
+
+function add(num1, num2) {
+    return num1 + num2
+}
+
+add(x, y);
+
+const arrowAdd = (num1, num2) => num1 + num2;
+
+arrowAdd(x, y);
+
+const myCar = {
+    color: "blue",
+    weight: 850,
+    model: "fiat",
+    start: () => "car started!",
+    doors: [1,2,3,4]
+}
+
+console.log("color:", myCar.color);
+
+console.log("start:", myCar.start());
+
+for (let door of myCar.doors) {
+    console.log("I'm door", door)
+}
+
+myCar;
+
+class User {
+  constructor(name){
+    this.name = name;
+  }
+  sayHello(){
+    return "Hello, I'm " + this.name;
+  }
+}
+
+let John = new User("John");
+John.sayHello();
