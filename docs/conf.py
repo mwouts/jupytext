f311812a31f3327ed9431fee7b4fd7f20cd97523
Marc Wouts <marc.wouts@gmail.com>
2019-06-20 22:13:31 +0200

    We don't have a "_static" folder

--- docs/conf.py
+++ docs/conf.py
@@ -56,7 +56,7 @@ master_doc = 'index'  # Makes `index.md` the main file
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
+html_static_path = []
 
 # Output file base name for HTML help builder.
 htmlhelp_basename = "jupytext"
