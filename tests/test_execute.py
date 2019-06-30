e7cafe5054680ed394f956fa886e339d372e474f
Marc Wouts <marc.wouts@gmail.com>
2019-06-30 19:41:36 +0200

    New --execute option in jupytext
    Fixes #231

--- tests/test_execute.py
+++ tests/test_execute.py
@@ -35,3 +35,35 @@ def test_pipe_nbconvert_execute_sync(tmpdir):
     nb = read(tmp_ipynb)
     assert len(nb.cells) == 1
     assert nb.cells[0].outputs[0]['data'] == {'text/plain': '3'}
+
+
+@requires_nbconvert
+def test_execute(tmpdir):
+    tmp_ipynb = str(tmpdir.join('notebook.ipynb'))
+    tmp_py = str(tmpdir.join('notebook.py'))
+
+    with open(tmp_py, 'w') as fp:
+        fp.write("""1 + 2
+""")
+
+    jupytext(args=[tmp_py, '--to', 'ipynb', '--execute'])
+
+    nb = read(tmp_ipynb)
+    assert len(nb.cells) == 1
+    assert nb.cells[0].outputs[0]['data'] == {'text/plain': '3'}
+
+
+@requires_nbconvert
+def test_execute_sync(tmpdir):
+    tmp_ipynb = str(tmpdir.join('notebook.ipynb'))
+    tmp_py = str(tmpdir.join('notebook.py'))
+
+    with open(tmp_py, 'w') as fp:
+        fp.write("""1 + 2
+""")
+
+    jupytext(args=[tmp_py, '--set-formats', 'py,ipynb', '--sync', '--execute'])
+
+    nb = read(tmp_ipynb)
+    assert len(nb.cells) == 1
+    assert nb.cells[0].outputs[0]['data'] == {'text/plain': '3'}
