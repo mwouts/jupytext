822a1d3ead470835f4fdf36a5c6e9aba24cc75e8
Marc Wouts <marc.wouts@gmail.com>
2019-07-17 23:59:31 +0200

    Version 1.0.1 of the jupyterlab-jupytext extension
    - A click on a selected format toggles the pairing (#289)
    - The extension now uses `JupyterFrontEnd` and `JupyterFrontEndPlugin` from `@jupyterlab/application` rather than `JupyterLab` and `JupyterLabPlugin` for compatibility with JupyterLab 1.0.

---
+++ packages/labextension/CHANGELOG.md
@@ -0,0 +1,16 @@
+# 1.0.1 (2019-07-18)
+
+- A click on a selected format toggle the pairing (#289)
+- Use `JupyterFrontEnd` and `JupyterFrontEndPlugin` from `@jupyterlab/application` rather than `JupyterLab` and `JupyterLabPlugin` for compatibility with JupyterLab 1.0.
+
+# 1.0.0 (2019-07-06)
+
+- First extension compatible with JupyterLab 1.0
+
+# 0.19.0 (2019-07-06)
+
+- Last extension compatible with JupyterLab 0.35
+
+# 0.1.0 (2018-10-02)
+
+- Initial release of the extension
--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
                                                          'jupytext/nbextension/jupytext_menu.png',
                                                          'jupytext/nbextension/jupytext_menu_zoom.png',
                                                          'jupytext/nbextension/jupytext.yml']),
-                ('share/jupyter/lab/extensions', ['packages/labextension/jupyterlab-jupytext-1.0.0.tgz'])],
+                ('share/jupyter/lab/extensions', ['packages/labextension/jupyterlab-jupytext-1.0.1.tgz'])],
     entry_points={'console_scripts': ['jupytext = jupytext.cli:jupytext_cli']},
     tests_require=['pytest'],
     install_requires=['nbformat>=4.0.0', 'pyyaml', 'mock;python_version<"3"'],
