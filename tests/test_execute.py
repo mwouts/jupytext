67ded3c267a8a35eb5a49b5fb95155e94e943ca8
Marc Wouts <marc.wouts@gmail.com>
2019-09-01 12:38:17 +0200

    Fix test on pypy
    #303

--- tests/test_execute.py
+++ tests/test_execute.py
@@ -84,7 +84,7 @@ a + 1
 """)
 
     import nbconvert
-    with pytest.raises(nbconvert.preprocessors.execute.CellExecutionError, match="NameError: name 'a' is not defined"):
+    with pytest.raises(nbconvert.preprocessors.execute.CellExecutionError, match="is not defined"):
         jupytext(args=[tmp_md, '--execute'])
 
 
