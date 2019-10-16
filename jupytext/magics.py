790dad82eeaf6221c1c4d399326d48aad3ade362
Marc Wouts <marc.wouts@gmail.com>
2019-10-17 00:49:23 +0200

    Simplify the is_magic function

--- jupytext/magics.py
+++ jupytext/magics.py
@@ -25,16 +25,18 @@ _PYTHON_MAGIC_CMD = re.compile(r"^(# |#)*({})($|\s$|\s[^=,])".format('|'.join(
     # windows
     ['copy', 'ddir', 'echo', 'ls', 'ldir', 'mkdir', 'ren', 'rmdir'])))
 
+_SCRIPT_LANGUAGES = [_SCRIPT_EXTENSIONS[ext]['language'] for ext in _SCRIPT_EXTENSIONS]
+
 
 def is_magic(line, language, global_escape_flag=True):
     """Is the current line a (possibly escaped) Jupyter magic, and should it be commented?"""
-    if language in ['octave', 'matlab']:
+    if language in ['octave', 'matlab'] or language not in _SCRIPT_LANGUAGES:
         return False
-    if _MAGIC_FORCE_ESC_RE.get(language, _MAGIC_FORCE_ESC_RE['python']).match(line):
+    if _MAGIC_FORCE_ESC_RE[language].match(line):
         return True
-    if not global_escape_flag or _MAGIC_NOT_ESC_RE.get(language, _MAGIC_NOT_ESC_RE['python']).match(line):
+    if not global_escape_flag or _MAGIC_NOT_ESC_RE[language].match(line):
         return False
-    if _MAGIC_RE.get(language, _MAGIC_RE['python']).match(line):
+    if _MAGIC_RE[language].match(line):
         return True
     if language != 'python':
         return False
