2fb6ceef58320045c6166fdd31b7a4489efc3095
Marc Wouts <marc.wouts@gmail.com>
2019-10-20 22:48:45 +0200

    auto_ext computed from kernelspec metadata is language_info is missing
    Jupytext files often don't include the language_info metadata #358

--- tests/test_auto_ext.py
+++ tests/test_auto_ext.py
@@ -18,8 +18,22 @@ def test_auto_in_fmt(nb_file):
     assert nb2.metadata['jupytext']['text_representation']['format_name'] == 'percent'
 
     del nb.metadata['language_info']
+    del nb.metadata['kernelspec']
     with pytest.raises(JupytextFormatError):
         writes(nb, 'auto:percent')
+
+
+@pytest.mark.parametrize('nb_file', list_notebooks('ipynb_all'))
+def test_auto_from_kernelspecs_works(nb_file):
+    nb = read(nb_file)
+    language_info = nb.metadata.pop('language_info')
+    expected_ext = language_info.get('file_extension')
+    if not expected_ext:
+        pytest.skip('No file_extension in language_info')
+    if expected_ext == '.r':
+        expected_ext = '.R'
+    auto_ext = auto_ext_from_metadata(nb.metadata)
+    assert auto_ext == expected_ext
 
 
 @pytest.mark.parametrize('nb_file', list_notebooks('ipynb_R') + list_notebooks('ipynb_py', skip='(World|plotly)'))
@@ -40,6 +54,7 @@ def test_auto_in_formats(nb_file):
     assert nb2.metadata['jupytext']['formats'] == expected_formats
 
     del nb.metadata['language_info']
+    del nb.metadata['kernelspec']
     with pytest.raises(JupytextFormatError):
         writes(nb, 'ipynb')
     with pytest.raises(JupytextFormatError):
