aaa6fab5a2098286c91b2c7070a1302f05483935
Marc Wouts <marc.wouts@gmail.com>
2019-10-23 22:25:26 +0200

    Added mirror representation for the latex notebook
    #305

---
+++ tests/notebooks/mirror/ipynb_to_sphinx/Line_breaks_in_LateX_305.py
@@ -0,0 +1,42 @@
+# ---
+# jupyter:
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+"""
+This cell uses no particular cell marker
+
+$$
+\begin{align}
+\dot{x} & = \sigma(y-x)\\
+\dot{y} & = \rho x - y - xz \\
+\dot{z} & = -\beta z + xy
+\end{align}
+$$
+"""
+
+###############################################################################
+# This cell uses no particular cell marker, and a single slash in the $\LaTeX$ equation
+#
+# $$
+# \begin{align}
+# \dot{x} & = \sigma(y-x) \
+# \dot{y} & = \rho x - y - xz \
+# \dot{z} & = -\beta z + xy
+# \end{align}
+# $$
+
+'''
+This cell uses the triple quote cell markers introduced at https://github.com/mwouts/jupytext/issues/305
+
+$$
+\begin{align}
+\dot{x} & = \sigma(y-x)\\
+\dot{y} & = \rho x - y - xz \\
+\dot{z} & = -\beta z + xy
+\end{align}
+$$
+'''
