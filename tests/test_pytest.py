3cbf2649611fd856885281f4b9153ea027fcafc7
Marc Wouts <marc.wouts@gmail.com>
2019-10-31 00:47:44 +0100

    test pytest on a notebook

---
+++ tests/test_pytest.py
@@ -0,0 +1,32 @@
+import pytest
+from jupytext.cli import jupytext
+
+
+def test_run_pytest_ok(
+        tmpdir,
+        text="""This is a notebook with a test that is supposed to pass
+```python
+def test_ok():
+    assert True
+```
+"""):
+    tmp_md = str(tmpdir.join('notebook.md'))
+    with open(tmp_md, 'w') as fp:
+        fp.write(text)
+    jupytext([tmp_md, '--check', 'pytest'])
+
+
+def test_run_pytest_fail(
+        tmpdir,
+        capsys,
+        text="""This is a notebook with a test that should not pass
+```python
+def test_fail():
+    assert False
+```
+"""):
+    tmp_md = str(tmpdir.join('notebook.md'))
+    with open(tmp_md, 'w') as fp:
+        fp.write(text)
+    with pytest.raises(SystemExit):
+        jupytext([tmp_md, '--check', 'pytest'])
