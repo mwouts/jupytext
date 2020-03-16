17267a2f6a28fede745d7c479f8e21fb8be8a2d4
Chris Sewell <chrisj_sewell@hotmail.com>
2020-03-16 06:03:52 +0000

    add MystMetadataParsingError and tests

---
+++ tests/test_ipynb_to_myst.py
@@ -0,0 +1,57 @@
+from textwrap import dedent
+import pytest
+from jupytext.myst import myst_to_notebook, CODE_DIRECTIVE, MystMetadataParsingError
+
+
+def test_bad_notebook_metadata():
+    """Test exception raised if notebook metadata cannot be parsed."""
+    with pytest.raises(MystMetadataParsingError):
+        myst_to_notebook(
+            dedent(
+                """\
+            ---
+            {{a
+            ---
+            """
+            )
+        )
+
+
+def test_bad_code_metadata():
+    """Test exception raised if cell metadata cannot be parsed."""
+    with pytest.raises(MystMetadataParsingError):
+        myst_to_notebook(
+            dedent(
+                """\
+            ```{{{0}}}
+            ---
+            {{a
+            ---
+            ```
+            """
+            ).format(CODE_DIRECTIVE)
+        )
+
+
+def test_bad_markdown_metadata():
+    """Test exception raised if markdown metadata cannot be parsed."""
+    with pytest.raises(MystMetadataParsingError):
+        myst_to_notebook(
+            dedent(
+                """\
+            +++ {{a
+            """
+            )
+        )
+
+
+def test_bad_markdown_metadata2():
+    """Test exception raised if markdown metadata is not a dict."""
+    with pytest.raises(MystMetadataParsingError):
+        myst_to_notebook(
+            dedent(
+                """\
+            +++ [1, 2]
+            """
+            )
+        )
