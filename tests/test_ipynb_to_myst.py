2929eaa132c8b79bc4320dfe8afae679c088ece2
Chris Sewell <chrisj_sewell@hotmail.com>
2020-03-16 06:11:24 +0000

    fix python 3.5 tests

--- tests/test_ipynb_to_myst.py
+++ tests/test_ipynb_to_myst.py
@@ -1,8 +1,10 @@
 from textwrap import dedent
 import pytest
 from jupytext.myst import myst_to_notebook, CODE_DIRECTIVE, MystMetadataParsingError
+from .utils import requires_myst
 
 
+@requires_myst
 def test_bad_notebook_metadata():
     """Test exception raised if notebook metadata cannot be parsed."""
     with pytest.raises(MystMetadataParsingError):
@@ -17,6 +19,7 @@ def test_bad_notebook_metadata():
         )
 
 
+@requires_myst
 def test_bad_code_metadata():
     """Test exception raised if cell metadata cannot be parsed."""
     with pytest.raises(MystMetadataParsingError):
@@ -33,6 +36,7 @@ def test_bad_code_metadata():
         )
 
 
+@requires_myst
 def test_bad_markdown_metadata():
     """Test exception raised if markdown metadata cannot be parsed."""
     with pytest.raises(MystMetadataParsingError):
@@ -45,6 +49,7 @@ def test_bad_markdown_metadata():
         )
 
 
+@requires_myst
 def test_bad_markdown_metadata2():
     """Test exception raised if markdown metadata is not a dict."""
     with pytest.raises(MystMetadataParsingError):
