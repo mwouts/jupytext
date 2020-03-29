57c9b074648ae5bd71a23fd9472af75578094840
Marc Wouts <marc.wouts@gmail.com>
2020-03-29 15:03:51 +0200

    New rules for combining outputs with input cells

--- jupytext/combine.py
+++ jupytext/combine.py
@@ -19,18 +19,15 @@ def black_invariant(text, chars=None):
     return text
 
 
-def same_content(ref, test):
+def same_content(ref, test, endswith=False):
     """Is the content of two cells the same, up to reformating by black"""
+    if endswith:
+        return black_invariant(ref).endswith(black_invariant(test))
     return black_invariant(ref) == black_invariant(test)
 
 
 def combine_inputs_with_outputs(nb_source, nb_outputs, fmt=None):
-    """Copy outputs and metadata of the second notebook into
-    the first one, for cells that have matching inputs"""
-
-    output_code_cells = [cell for cell in nb_outputs.cells if cell.cell_type == 'code']
-    output_other_cells = [cell for cell in nb_outputs.cells if cell.cell_type != 'code']
-
+    """Copy outputs and metadata of the second notebook into the first one."""
     # nbformat version number taken from the notebook with outputs
     assert nb_outputs.nbformat == nb_source.nbformat, \
         "The notebook with outputs is in format {}.{}, please upgrade it to {}.x".format(
@@ -60,33 +57,89 @@ def combine_inputs_with_outputs(nb_source, nb_outputs, fmt=None):
     else:
         cell_metadata_filter = nb_source.metadata.get('jupytext', {}).get('cell_metadata_filter')
 
-    for cell in nb_source.cells:
+    outputs_map = map_outputs_to_inputs(nb_source.cells, nb_outputs.cells)
+
+    for cell, j in zip(nb_source.cells, outputs_map):
         # Remove outputs to warranty that trust of returned notebook is that of second notebook
         if cell.cell_type == 'code':
             cell.execution_count = None
             cell.outputs = []
 
-            for i, ocell in enumerate(output_code_cells):
-                if same_content(cell.source, ocell.source):
-                    # Fill outputs with that of second notebook
-                    cell.execution_count = ocell.execution_count
-                    cell.outputs = ocell.outputs
+            if j is not None:
+                ocell = nb_outputs.cells[j]
+                cell.execution_count = ocell.execution_count
+                cell.outputs = ocell.outputs
 
-                    # Restore the filtered output cell metadata
-                    cell.metadata = restore_filtered_metadata(cell.metadata, ocell.metadata,
-                                                              cell_metadata_filter, _IGNORE_CELL_METADATA)
-
-                    output_code_cells = output_code_cells[(i + 1):]
-                    break
+                # Restore the filtered output cell metadata
+                cell.metadata = restore_filtered_metadata(cell.metadata, ocell.metadata,
+                                                          cell_metadata_filter, _IGNORE_CELL_METADATA)
         else:
-            for i, ocell in enumerate(output_other_cells):
-                if cell.cell_type == ocell.cell_type and same_content(cell.source, ocell.source):
-                    # The 'spin' format does not allow metadata on non-code cells
-                    cell.metadata = restore_filtered_metadata(cell.metadata, ocell.metadata,
-                                                              '-all' if format_name == 'spin' else cell_metadata_filter,
-                                                              _IGNORE_CELL_METADATA)
+            if j is not None:
+                ocell = nb_outputs.cells[j]
 
-                    output_other_cells = output_other_cells[(i + 1):]
-                    break
+                # The 'spin' format does not allow metadata on non-code cells
+                cell.metadata = restore_filtered_metadata(cell.metadata, ocell.metadata,
+                                                          '-all' if format_name == 'spin' else cell_metadata_filter,
+                                                          _IGNORE_CELL_METADATA)
 
     return nb_source
+
+
+def map_outputs_to_inputs(cells_inputs, cells_outputs):
+    """Returns a map i->(j or None) that maps the cells with outputs to the input cells"""
+    n_in = len(cells_inputs)
+    n_out = len(cells_outputs)
+    outputs_map = [None] * n_in
+
+    # First rule: match based on cell type, content, in increasing order
+    current_j = 0
+    for i in range(n_in):
+        cell_input = cells_inputs[i]
+        for j in range(current_j, n_out):
+            cell_output = cells_outputs[j]
+            if cell_input.cell_type == cell_output.cell_type and same_content(cell_input.source, cell_output.source):
+                outputs_map[i] = j
+                current_j = j + 1
+                break
+
+    # Second rule: match unused outputs based on cell type and content
+    # Third rule: is the new cell the final part of a previous cell with outputs?
+    unused_ouputs = set(range(n_out)).difference(outputs_map)
+    for endswith in [False, True]:
+        if not unused_ouputs:
+            return outputs_map
+
+        for i in range(n_in):
+            if outputs_map[i] is not None:
+                continue
+            cell_input = cells_inputs[i]
+            for j in unused_ouputs:
+                cell_output = cells_outputs[j]
+                if cell_input.cell_type == cell_output.cell_type and \
+                        same_content(cell_output.source, cell_input.source, endswith):
+                    outputs_map[i] = j
+                    unused_ouputs.remove(j)
+                    break
+
+    # Fourth rule: match based on increasing index, for non-empty cells
+    if not unused_ouputs:
+        return outputs_map
+
+    prev_j = -1
+    for i in range(n_in):
+        if outputs_map[i] is not None:
+            prev_j = outputs_map[i]
+            continue
+
+        j = prev_j + 1
+        if j not in unused_ouputs:
+            continue
+
+        cell_input = cells_inputs[i]
+        cell_output = cells_outputs[j]
+        if cell_input.cell_type == cell_output.cell_type and cell_input.source:
+            outputs_map[i] = j
+            unused_ouputs.remove(j)
+            prev_j = j
+
+    return outputs_map
