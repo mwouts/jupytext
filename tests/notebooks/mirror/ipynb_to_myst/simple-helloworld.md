3d2dff773e6a10bcb883d4e2f9e239b4026c8402
Przemek Wesołek <przemek@wesolek.net>
2020-04-26 18:06:46 +0000

    Added a sample Java notebook to tests

---
+++ tests/notebooks/mirror/ipynb_to_myst/simple-helloworld.md
@@ -0,0 +1,30 @@
+---
+jupytext:
+  encoding: '// -*- coding: utf-8 -*-'
+  formats: ipynb,java:light
+kernelspec:
+  display_name: Java
+  language: java
+  name: java
+---
+
+Let's define some class.
+
+```{code-cell}
+class A {
+    public void hello() {
+        System.out.println("Hello World");
+    }   
+}
+```
+
+And now we call its method.
+
+```{code-cell}
+new A().hello();
+```
+
+You can run it e.g. with `jshell`
+
+* from command line, as `jshell simple-helloworld.java`
+* from jshell's shell with `/open simple-helloworld.java`
---
+++ tests/notebooks/mirror/ipynb_to_percent/simple-helloworld.java
@@ -0,0 +1,32 @@
+// -*- coding: utf-8 -*-
+// ---
+// jupyter:
+//   jupytext:
+//     formats: ipynb,java:percent
+//   kernelspec:
+//     display_name: Java
+//     language: java
+//     name: java
+// ---
+
+// %% [markdown]
+// Let's define some class.
+
+// %%
+class A {
+    public void hello() {
+        System.out.println("Hello World");
+    }   
+}
+
+// %% [markdown]
+// And now we call its method.
+
+// %%
+new A().hello();
+
+// %% [markdown]
+// You can run it e.g. with `jshell`
+//
+// * from command line, as `jshell simple-helloworld.java`
+// * from jshell's shell with `/open simple-helloworld.java`
