c9f03b79c6f1b5562dec34b52499218ad0216039
Marc Wouts <marc.wouts@gmail.com>
2020-05-01 02:25:49 +0200

    TOML as an optional dependency

--- setup.py
+++ setup.py
@@ -55,7 +55,10 @@ setup(
     entry_points={"console_scripts": ["jupytext = jupytext.cli:jupytext"]},
     tests_require=["pytest"],
     install_requires=["nbformat>=4.0.0", "pyyaml", 'mock;python_version<"3"'],
-    extras_require={"myst": ["myst-parser~=0.8; python_version >= '3.6'"]},
+    extras_require={
+        "myst": ["myst-parser~=0.8; python_version >= '3.6'"],
+        "toml": ["toml"],
+    },
     license="MIT",
     classifiers=[
         "Development Status :: 5 - Production/Stable",
