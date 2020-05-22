67d43e9801bf105ccd147fcb182a6c1f082ae8d6
Marc Wouts <marc.wouts@gmail.com>
2020-05-22 13:01:24 +0200

    JUPYTEXT_CEILING_DIRECTORIES

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,5 @@
 import pytest
+import jupytext
 
 try:
     import unittest.mock as mock
@@ -10,3 +11,8 @@ except ImportError:
 def no_jupytext_version_number():
     with mock.patch("jupytext.header.INSERT_AND_CHECK_VERSION_NUMBER", False):
         yield
+
+
+# Pytest's tmpdir is in /tmp (at least for me), so this helps avoiding interferences between
+# global configuration on HOME and the test collection
+jupytext.config.JUPYTEXT_CEILING_DIRECTORIES = ["/tmp/"]
