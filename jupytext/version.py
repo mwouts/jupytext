9d19eb3a33418afbd51a1f85e976ed32deaf8e52
Marc Wouts <marc.wouts@gmail.com>
2020-11-30 21:40:57 +0100

    Drop explicit support for Python 2.7

--- jupytext/version.py
+++ jupytext/version.py
@@ -1,3 +1,3 @@
 """Jupytext's version number"""
 
-__version__ = "1.7.1"
+__version__ = "1.8.0-dev"
--- setup.py
+++ setup.py
@@ -81,7 +81,6 @@ setup(
         "Intended Audience :: Science/Research",
         "Topic :: Text Processing :: Markup",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
