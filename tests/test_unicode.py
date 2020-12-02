421b0fdede65a620c7afb210dd316c9a39a716a2
Marc Wouts <marc.wouts@gmail.com>
2020-12-02 12:56:08 +0100

    Drop references to "unicode" (Python 2)

--- tests/test_unicode.py
+++ tests/test_unicode.py
@@ -5,18 +5,13 @@ from jupytext.compare import compare
 from .utils import list_notebooks
 from nbformat.v4.nbbase import new_notebook, new_markdown_cell
 
-try:
-    unicode  # Python 2
-except NameError:
-    unicode = str  # Python 3
-
 
 @pytest.mark.parametrize("nb_file", list_notebooks() + list_notebooks("Rmd"))
 def test_notebook_contents_is_unicode(nb_file):
     nb = jupytext.read(nb_file)
 
     for cell in nb.cells:
-        assert cell.source == "" or isinstance(cell.source, unicode)
+        assert isinstance(cell.source, str)
 
 
 def test_write_non_ascii(tmpdir):
