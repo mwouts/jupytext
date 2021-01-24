07feede9c3a9da67d207fa73780fdce57adddf94
Marc Wouts <marc.wouts@gmail.com>
2021-01-24 03:41:54 +0100

    Encapsulate code cells with triple backticks with 4 backticks in Markdown

---
+++ tests/test_markdown_in_code_cells.py
@@ -0,0 +1,81 @@
+"""Issue #712"""
+from nbformat.v4.nbbase import new_code_cell, new_notebook
+
+from jupytext import reads, writes
+from jupytext.compare import compare, compare_notebooks
+
+
+def test_triple_backticks_in_code_cell(
+    no_jupytext_version_number,
+    nb=new_notebook(
+        metadata={"main_language": "python"},
+        cells=[
+            new_code_cell(
+                '''a = """
+```
+foo
+```
+"""'''
+            )
+        ],
+    ),
+    text='''---
+jupyter:
+  jupytext:
+    main_language: python
+---
+
+````python
+a = """
+```
+foo
+```
+"""
+````
+''',
+):
+    actual_text = writes(nb, fmt="md")
+    compare(actual_text, text)
+
+    actual_nb = reads(text, fmt="md")
+    compare_notebooks(actual_nb, nb)
+
+
+def test_alternate_tree_four_five_backticks(
+    no_jupytext_version_number,
+    nb=new_notebook(
+        metadata={"main_language": "python"},
+        cells=[
+            new_code_cell('a = """\n```\n"""'),
+            new_code_cell("b = 2"),
+            new_code_cell('c = """\n````\n"""'),
+        ],
+    ),
+    text='''---
+jupyter:
+  jupytext:
+    main_language: python
+---
+
+````python
+a = """
+```
+"""
+````
+
+```python
+b = 2
+```
+
+`````python
+c = """
+````
+"""
+`````
+''',
+):
+    actual_text = writes(nb, fmt="md")
+    compare(actual_text, text)
+
+    actual_nb = reads(text, fmt="md")
+    compare_notebooks(actual_nb, nb)
