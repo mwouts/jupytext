74ca66832b4e18e8ff852691a964843115efab66
Marc Wouts <marc.wouts@gmail.com>
2021-11-30 21:48:33 +0100

    Display the errors raised by 'quarto'

--- jupytext/quarto.py
+++ jupytext/quarto.py
@@ -22,12 +22,13 @@ def quarto(args, filein=None):
     if filein:
         cmd.append(filein)
 
-    proc = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+    proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     out, err = proc.communicate()
     if proc.returncode:
         raise QuartoError(
-            f"{' '.join(cmd)} exited with return code {proc.returncode}\n{err}"
+            f"{' '.join(cmd)} exited with return code {proc.returncode}\n{err.decode('utf-8')}"
         )
+
     return out.decode("utf-8")
 
 
