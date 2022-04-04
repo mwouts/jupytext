b2a2ab2393fcffa866d5821cb5a46f8510990152
Marc Wouts <marc.wouts@gmail.com>
2022-04-04 22:33:34 +0200

    Ignore non-coverage of exception (#947)

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1167,12 +1167,13 @@ def no_warning():
 
     # There should be no warning
     for record in records:
-        # Temporary exception for for this one, see #865
+        # Temporary exception for this one, see #865
+        # (does not seem to not occur anymore, as of April 2022, see #947)
         if (
             "Passing a schema to Validator.iter_errors is deprecated "
             "and will be removed in a future release" in str(record.message)
         ):
-            continue
+            continue  # pragma: no cover
         raise RuntimeError(record)
 
 
