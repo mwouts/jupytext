bafca2c64f1edf48c4dd7e96917f6243090da233
Marc Wouts <marc.wouts@gmail.com>
2022-10-30 22:53:35 +0000

    Jupytext is tested against Python 3.10 and 3.11 on the CI

--- setup.py
+++ setup.py
@@ -60,6 +60,8 @@ setup_args = dict(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
     ],
 )
 
