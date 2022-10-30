13f26702ec0ec2f45b7aeac01a06b72a91d98cb9
Marc Wouts <marc.wouts@gmail.com>
2022-10-30 21:46:18 +0000

    Skip test if recent nbformat is not available

--- tests/test_sample_notebooks_are_normalized.py
+++ tests/test_sample_notebooks_are_normalized.py
@@ -6,6 +6,7 @@ import jupytext
 from .utils import list_notebooks
 
 
+@pytest.mark.skipif(nbformat.__version__ <= "5.7", reason="normalize is not available")
 @pytest.mark.parametrize("nb_file", list_notebooks("all", skip="(invalid|pyc)"))
 def test_sample_notebooks_are_normalized(nb_file):
     nb = jupytext.read(nb_file)
