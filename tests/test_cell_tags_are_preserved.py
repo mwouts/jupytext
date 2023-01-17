ae382eb5def6a33f580be0ee71b8f62241cb6f77
Marc Wouts <marc.wouts@gmail.com>
2023-01-17 11:37:20 +0000

    Add a few tests to make sure that tags are supported in the main Jupytext formats

---
+++ tests/test_cell_tags_are_preserved.py
@@ -0,0 +1,36 @@
+import pytest
+from nbformat.v4.nbbase import new_code_cell, new_markdown_cell
+
+from jupytext import reads, writes
+from jupytext.formats import JUPYTEXT_FORMATS
+
+
+@pytest.fixture()
+def notebook_with_tags(python_notebook):
+    nb = python_notebook
+    nb.cells = [
+        new_code_cell("1 + 1", metadata={"tags": ["tag1"]}),
+        new_markdown_cell("some text", metadata={"tags": ["tag2"]}),
+    ]
+    return nb
+
+
+def formats_with_support_for_cell_metadata():
+    for fmt in JUPYTEXT_FORMATS:
+        if fmt.format_name not in ["sphinx", "nomarker", "spin", "quarto"]:
+            yield f"{fmt.extension[1:]}:{fmt.format_name}"
+
+
+@pytest.mark.parametrize(
+    "fmt", ["py:percent", "py:light", "md:markdown", "md:myst", "Rmd:rmarkdown"]
+)
+def test_main_formats_support_cell_metadata(fmt):
+    assert fmt in set(formats_with_support_for_cell_metadata())
+
+
+@pytest.mark.parametrize("fmt", formats_with_support_for_cell_metadata())
+def test_tags_are_preserved(notebook_with_tags, fmt):
+    text = writes(notebook_with_tags, fmt)
+    nb = reads(text, fmt)
+    assert nb.cells[0]["metadata"]["tags"] == ["tag1"]
+    assert nb.cells[1]["metadata"]["tags"] == ["tag2"]
