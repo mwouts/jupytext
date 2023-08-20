8cddcb263af669908ddd89e57bef1128d460a2b0
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2023-08-20 12:28:26 +0200

    a little simpler, no need for a callable plus, hopefully made pre-commit happy

--- jupytext/jupytext_config.py
+++ jupytext/jupytext_config.py
@@ -11,37 +11,41 @@ class SubCommand:
         self.name = name
         self.help = help
         SUBCOMMANDS.append(self)
+
     def main(self, args):
         """
         return 0 if all goes well
         """
-        print(f"redefine to implement this subcommand")
-    def __call__(self, args):
-        return self.main(args)
+        print(
+            f"{self.__class__.__name__}: redefine main() to implement this subcommand"
+        )
+        return 1
 
 
 class List(SubCommand):
     def __init__(self):
-        super().__init__("list",
-                         "Display current settings in labconfig/")
+        super().__init__("list", "Display current settings in labconfig/")
+
     def main(self, args):
         LabConfig().read().list()
         return 0
+
     def fill_parser(self, subparser):
         pass
 
 
 class SetDefaultViewer(SubCommand):
     def __init__(self):
-        super().__init__("set-default-viewer",
-                         "Set default viewers for JupyterLab")
+        super().__init__("set-default-viewer", "Set default viewers for JupyterLab")
+
     def main(self, args):
         LabConfig().read().set_default_viewers(args.language).write()
         return 0
+
     def fill_parser(self, subparser):
         subparser.add_argument(
-            "language", nargs='*',
-            help="the language(s) that the viewer applies to")
+            "language", nargs="*", help="the language(s) that the viewer applies to"
+        )
 
 
 # create the subcommands
@@ -54,8 +58,7 @@ def main():
     subparsers = parser.add_subparsers(required=True)
     for subcommand in SUBCOMMANDS:
         subparser = subparsers.add_parser(subcommand.name, help=subcommand.help)
-        subparser.set_defaults(func=subcommand)
+        subparser.set_defaults(subcommand=subcommand)
         subcommand.fill_parser(subparser)
     args = parser.parse_args()
-    # calling the subcommand object since it's a callable
-    return args.func(args)
+    return args.subcommand.main(args)
