ebf7a8b070cc48c79612199a6048e29a20dfc55c
mahendrapaipuri <mahendra.paipuri@gmail.com>
2023-11-06 17:45:41 +0100

    test: Use a pixel tolerance on screenshot comparison
    - Comparing colors can be a bit flaky given colors can vary across platforms
    
    - We use a maxDiffPixelRatio to smooth these issues out of tests

--- jupyterlab/packages/jupyterlab-jupytext-extension/ui-tests/playwright.config.js
+++ jupyterlab/packages/jupyterlab-jupytext-extension/ui-tests/playwright.config.js
@@ -5,10 +5,20 @@ const baseConfig = require('@jupyterlab/galata/lib/playwright-config');
 
 module.exports = {
   ...baseConfig,
+  // Visual comparison of screenshots can be flaky. Use a tolerance
+  expect: {
+    toMatchSnapshot: {
+      maxDiffPixelRatio: 0.02,
+    },
+  },
   webServer: {
     command: 'jlpm start',
     url: 'http://localhost:8888/lab',
     timeout: 120 * 1000,
     reuseExistingServer: !process.env.CI,
   },
+  // Switch to 'always' to keep raw assets for all tests
+  preserveOutput: 'failures-only', // Breaks HTML report if use.video == 'on'
+  // Try one retry as some tests are flaky
+  // retries: process.env.CI ? 1 : 0,
 };
