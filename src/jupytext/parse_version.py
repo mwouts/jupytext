c1850be75f446f88cdf9fc777359d50d08dd7e92
Marc Wouts <marc.wouts@gmail.com>
2023-11-11 21:51:57 +0000

    Use packaging.version if available

--- src/jupytext/parse_version.py
+++ src/jupytext/parse_version.py
@@ -1,11 +1,10 @@
 def parse_version(version, custom_error=ImportError):
     try:
-        from pkg_resources import parse_version as parse
+        from packaging.version import parse
     except ImportError:
         try:
-            from packaging.version import parse
+            from pkg_resources import parse_version as parse
         except ImportError:
-            raise custom_error("Please install either pkg_resources or packaging")
+            raise custom_error("Please install either packaging or pkg_resources")
 
-    print(version)
     return parse(version)
