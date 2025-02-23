1921e4e766e4e2c79578cc028ef0110f6da6dfa3
bcapuano <bcapuano@asu.edu>
2025-02-23 14:30:20 -0700

    Updated path in build.sh for corpora

--- tests/fuzzing/build.sh
+++ tests/fuzzing/build.sh
@@ -8,5 +8,5 @@ for fuzzer in $(find tests/fuzzing -name '*_fuzzer.py');do
   compile_python_fuzzer "$fuzzer"
 done
 
-zip -q $OUT/rw_fuzzer_seed_corpus.zip fuzz/corpus/*
-zip -q $OUT/roundtrip_fuzzer_seed_corpus.zip fuzz/corpus/*
+zip -q $OUT/rw_fuzzer_seed_corpus.zip tests/fuzzing/corpus/*
+zip -q $OUT/roundtrip_fuzzer_seed_corpus.zip tests/fuzzing/corpus/*
