73b146ccf46807d3b280f277954a8640713055b7
Marc Wouts <marc.wouts@gmail.com>
2025-10-11 19:29:32 +0100

    Add an example notebook with complex YAML-like content

---
+++ tests/data/notebooks/outputs/ipynb_to_script_vim_folding_markers/raw_cell_with_complex_yaml_like_content.py
@@ -0,0 +1,16 @@
+# ---
+#
+# This is a complex paragraph
+# that is split over multiple lines.
+#
+# It also includes blank lines.
+#
+#
+# jupyter:
+#   jupytext:
+#     cell_markers: '{{{,}}}'
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
